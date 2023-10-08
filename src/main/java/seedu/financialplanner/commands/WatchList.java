5f7d5ba1fafae93224d2ae0559edae789e3747bd
wwweert123 <puachengxuan.fp@gmail.com>
2023-10-08 15:07:24 +0800

    Add simple JSON library to parse response body and print to UI

--- src/main/java/seedu/financialplanner/commands/WatchList.java
+++ src/main/java/seedu/financialplanner/commands/WatchList.java
@@ -8,6 +8,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.Iterator;
 
 public class WatchList extends Command {
     @Override
@@ -21,10 +22,32 @@ public class WatchList extends Command {
 
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
+            // System.out.println(response.body());
+            Object obj = new JSONParser().parse(response.body());
+
+            JSONArray ja = (JSONArray) obj;
+            // System.out.println(ja.toJSONString());
+            Iterator itr = ja.iterator();
+            System.out.print("Symbol");
+            System.out.print("  ");
+            System.out.print("Price");
+            System.out.print("     ");
+            System.out.print("Company Name");
+            System.out.println();
+            while (itr.hasNext()) {
+                JSONObject stock = (JSONObject) itr.next();
+                System.out.print(stock.get("symbol"));
+                System.out.print("    ");
+                System.out.print(stock.get("price"));
+                System.out.print("    ");
+                System.out.print(stock.get("name"));
+                System.out.println();
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
             throw new RuntimeException(e);
         }
 
