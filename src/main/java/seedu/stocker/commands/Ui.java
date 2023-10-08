5bf20560cb92c584383123cf1ba209e1943da17b
TeoHaoZhi <e0957449@u.nus.edu>
2023-10-08 17:23:41 +0800

    Add Junit test for help feature

--- src/main/java/seedu/stocker/commands/Ui.java
+++ src/main/java/seedu/stocker/commands/Ui.java
@@ -16,21 +16,29 @@ public class Ui {
         System.out.println("4. find - Find a specific drug in the system");
         System.out.println("5. help - List all available commands");
 
-        System.out.println("\n");
+        System.out.println();
+        System.out.println();
 
         System.out.println("Here is the formatting for the commands");
 
         System.out.println("For add:");
-        System.out.println("add /n <drug name> /d <expiry date> /q <quantity>" + "\n");
+        System.out.println("add /n <drug name> /d <expiry date> /q <quantity>");
+        System.out.println();
 
         System.out.println("For delete:");
-        System.out.println("delete /n <drug name>" + "\n");
+        System.out.println("delete /n <drug name>");
+        System.out.println();
+
 
         System.out.println("For list:");
-        System.out.println("list" + "\n");
+        System.out.println("list");
+        System.out.println();
+
 
         System.out.println("For find:");
-        System.out.println("find <keyword>" + "\n");
+        System.out.println("find <keyword>");
+        System.out.println();
+
 
         System.out.println("For help:");
         System.out.println("help");
