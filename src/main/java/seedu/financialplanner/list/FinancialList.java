f8e310a44b9827b00635f829dd8d1fd8a11224e9
NeoMinWei <>
2023-10-08 12:35:33 +0800

    Create ArrayList to store entries

---
+++ src/main/java/seedu/financialplanner/list/FinancialList.java
@@ -0,0 +1,11 @@
+package seedu.financialplanner.list;
+
+import java.util.ArrayList;
+
+public class FinancialList {
+    protected ArrayList<Cashflow> list = new ArrayList<>();
+
+    public ArrayList<Cashflow> getList() {
+        return list;
+    }
+}
