bfa75c44ff77cc7824fb4a072c8d4a8e94a19e09
wwweert123 <puachengxuan.fp@gmail.com>
2023-10-08 15:15:11 +0800

    Use string format to create request URI

--- src/main/java/seedu/financialplanner/commands/WatchList.java
+++ src/main/java/seedu/financialplanner/commands/WatchList.java
@@ -1,5 +1,9 @@
 package seedu.financialplanner.commands;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import seedu.financialplanner.utils.Ui;
 
 import java.io.IOException;
@@ -7,16 +11,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
 import java.util.Iterator;
 
 public class WatchList extends Command {
+
+    private final String API_ENDPOINT = "https://financialmodelingprep.com/api/v3/quote/";
+    private final String API_KEY = "rNCNMmSLUR3BAyeKFHwN69QGzE8fmig1";
     @Override
     public void execute(Ui ui) {
 
         HttpClient client = HttpClient.newHttpClient();
+        String stocks = "META,AAPL";
+        String requestURI = String.format("%s%s?apikey=%s", API_ENDPOINT,stocks,API_KEY);
 
-        HttpRequest request = HttpRequest.newBuilder(URI.create("https://financialmodelingprep.com/api/v3/quote/META,AAPL?apikey=rNCNMmSLUR3BAyeKFHwN69QGzE8fmig1"))
+        HttpRequest request = HttpRequest.newBuilder(URI.create(requestURI))
                 .header("accept", "application/json")
                 .build();
 
