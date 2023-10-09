a311199b47d9c35e55f5509eaba762545283cd61
wwweert123 <puachengxuan.fp@gmail.com>
2023-10-09 14:32:21 +0800

    Create method in UI to print stock prices

--- src/main/java/seedu/financialplanner/utils/Ui.java
+++ src/main/java/seedu/financialplanner/utils/Ui.java
@@ -1,5 +1,8 @@
 package seedu.financialplanner.utils;
 
+import org.apache.commons.lang3.StringUtils;
+import org.json.simple.JSONObject;
+
 import java.util.Scanner;
 
 public class Ui {
@@ -21,5 +24,21 @@ public class Ui {
     public String input() {
         Scanner in = new Scanner(System.in);
         return in.nextLine().trim();
+    }
+
+    public void printWatchListHeader() {
+        System.out.print("Symbol");
+        System.out.print("    ");
+        System.out.print("Price");
+        System.out.print("     ");
+        System.out.print("Company Name");
+        System.out.println();
+    }
+
+    public void printStockInfo(JSONObject stock) {
+        String symbol = StringUtils.rightPad((String) stock.get("symbol"), 10);
+        String price = StringUtils.rightPad(stock.get("price").toString(), 10);
+        String name = StringUtils.rightPad((String) stock.get("name"), 10);
+        System.out.println(symbol + price + name);
     }
 }
