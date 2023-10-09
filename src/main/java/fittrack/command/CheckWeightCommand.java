5d4b5515c143fe1c0f60dd65b5e86c6930cb7c12
Faris Sirraj <faris@curium.sg>
2023-10-09 23:07:27 +0800

    Create class for checking the weight

---
+++ src/main/java/fittrack/command/CheckWeightCommand.java
@@ -0,0 +1,10 @@
+package fittrack.command;
+
+public class CheckWeightCommand extends Command {
+    public static final String COMMAND_WORD = "checkWeight";
+
+    @Override
+    public CommandResult execute() {
+        return null;
+    }
+}
