644bb97ee4809fec84ba24c2e6073e0988eb5a6a
danielpappa <daniel.pappalardo@icloud.com>
2023-10-09 21:50:16 +0800

    Create class Command

---
+++ src/main/java/seedu/cashleh/commands/Command.java
@@ -0,0 +1,25 @@
+package seedu.cashleh.commands;
+
+import seedu.cashleh.Income;
+import seedu.cashleh.IncomeStatement;
+
+public class Command {
+    protected IncomeStatement incomeStatement;
+    private int index;
+    public Command(int index) {
+        this.index = index;
+    }
+    public Command() {
+        //throw new UnsupportedOperationException("A generic command is not executable. Please define a subclass.");
+    }
+    public int getIndex() {
+        return this.index;
+    }
+    public Income getIncome() {
+        return incomeStatement.get(getIndex());
+    }
+    public void execute() {}
+    public void setIncomeStatement(IncomeStatement incomeStatement) {
+        this.incomeStatement = incomeStatement;
+    }
+}
