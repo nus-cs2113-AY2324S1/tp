223df0aaf53c99b427d96f0976496c4cb88987e3
danielpappa <daniel.pappalardo@icloud.com>
2023-10-09 21:49:16 +0800

    Rename main class into CashLeh and remove previous content

---
+++ src/main/java/seedu/cashleh/CashLeh.java
@@ -0,0 +1,12 @@
+package seedu.cashleh;
+
+import seedu.cashleh.commands.AddIncome;
+import seedu.cashleh.commands.DeleteIncome;
+import seedu.cashleh.commands.ViewIncome;
+
+import java.time.LocalDate;
+
+public class CashLeh {
+    public static void main(String[] args) {
+    }
+}
