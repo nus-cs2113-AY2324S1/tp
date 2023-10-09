a212cb420bd3cf786e68bbddb94452a5d41867af
Faris Sirraj <faris@curium.sg>
2023-10-09 23:07:02 +0800

    Create class for checking the height

---
+++ src/main/java/fittrack/command/CheckHeightCommand.java
@@ -0,0 +1,10 @@
+package fittrack.command;
+
+public class CheckHeightCommand extends Command{
+    public static final String COMMAND_WORD = "checkHeight";
+
+    @Override
+    public CommandResult execute() {
+        return null;
+    }
+}
