7bb04d887d4ece7439d62030cfa67a5d840825c7
Faris Sirraj <faris@curium.sg>
2023-10-09 23:06:35 +0800

    Create class for checking calorie surplus limit command

---
+++ src/main/java/fittrack/command/CheckCalorieSurplusLimitCommand.java
@@ -0,0 +1,10 @@
+package fittrack.command;
+
+public class CheckCalorieSurplusLimitCommand extends Command{
+    public static final String COMMAND_WORD = "checkCSL";
+
+    @Override
+    public CommandResult execute() {
+        return null;
+    }
+}
