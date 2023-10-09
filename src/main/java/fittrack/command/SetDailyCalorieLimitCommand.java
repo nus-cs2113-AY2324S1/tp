7e623f8cf9bc78fc3ebdae9068637c23022448a5
Faris Sirraj <faris@curium.sg>
2023-10-09 23:08:15 +0800

    Create class for setting the daily calorie limit

---
+++ src/main/java/fittrack/command/SetDailyCalorieLimitCommand.java
@@ -0,0 +1,11 @@
+package fittrack.command;
+
+public class SetDailyCalorieLimitCommand extends Command{
+    public static final String COMMAND_WORD = "setDCL";
+
+    @Override
+    public CommandResult execute() {
+        return null;
+    }
+}
+
