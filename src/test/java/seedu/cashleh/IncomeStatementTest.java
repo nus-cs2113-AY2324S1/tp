30ba3c34fb93c9b2b374634423f88e497812bde2
danielpappa <daniel.pappalardo@icloud.com>
2023-10-10 16:29:35 +0800

    Create basic JUnit tests for Income and IncomeStatement

---
+++ src/test/java/seedu/cashleh/IncomeStatementTest.java
@@ -0,0 +1,38 @@
+package seedu.cashleh;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IncomeStatementTest {
+    IncomeStatement testStatement = new IncomeStatement();
+    Income testIncome = new Income(200, "pocket money",
+            LocalDate.of(2023, 10, 10));
+    @Test
+    void getNumberOfEntries() {
+        assertEquals(testStatement.getNumberOfEntries(), 0);
+        testStatement.add(testIncome);
+        assertEquals(testStatement.getNumberOfEntries(), 1);
+        testStatement.add(testIncome);
+        assertEquals(testStatement.getNumberOfEntries(), 2);
+    }
+
+    @Test
+    void getSumOfEntries() {
+        testStatement.add(testIncome);
+        assertEquals(testStatement.getSumOfEntries(), 200);
+        testStatement.add(testIncome);
+        assertEquals(testStatement.getSumOfEntries(), 400);
+    }
+
+    @Test
+    void testToString() {
+        testStatement.add(testIncome);
+        testStatement.add(testIncome);
+        System.out.println(testStatement);
+        assertEquals(testStatement.toString(), "\t+ S$ 200: pocket money (date: 2023-10-10, One time)"
+        	+ "\n\t+ S$ 200: pocket money (date: 2023-10-10, One time)");
+    }
+}
---
+++ src/test/java/seedu/cashleh/IncomeTest.java
@@ -0,0 +1,24 @@
+package seedu.cashleh;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IncomeTest {
+    Income testIncome = new Income(200, "pocket money",
+            LocalDate.of(2023, 10, 10));
+
+    @Test
+    void isOneTimeIncome() {
+        assertEquals(testIncome.isOneTimeIncome(), "One time");
+        testIncome.setOneTimeIncome(false);
+        assertEquals(testIncome.isOneTimeIncome(), "Recurring");
+    }
+
+    @Test
+    void testToString() {
+        assertEquals(testIncome.toString(), "	+ S$ 200: pocket money (date: 2023-10-10, One time)");
+    }
+}
