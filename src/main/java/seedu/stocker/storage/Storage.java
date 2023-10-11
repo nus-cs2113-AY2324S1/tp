350fcc060811c2f4e25cb36254f2bd7845ea9338
TeoHaoZhi <e0957449@u.nus.edu>
2023-10-11 20:13:06 +0800

    Add methods to write to txt file

--- src/main/java/seedu/stocker/storage/Storage.java
+++ src/main/java/seedu/stocker/storage/Storage.java
@@ -1,4 +1,21 @@
 package seedu.stocker.storage;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Storage {
+
+    private static void writeToFile(String filePath, String textToAdd) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
+
+    private static void appendToFile(String filePath, String textToAppend) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
+        fw.write(textToAppend);
+        fw.close();
+    }
+
+
 }
