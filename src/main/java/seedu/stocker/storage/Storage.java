a46e5821d0bcebdf8cc07433cfb197f7fd91490b
TeoHaoZhi <e0957449@u.nus.edu>
2023-10-11 20:27:48 +0800

    Update method to append to txt file

--- src/main/java/seedu/stocker/storage/Storage.java
+++ src/main/java/seedu/stocker/storage/Storage.java
@@ -1,5 +1,6 @@
 package seedu.stocker.storage;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,10 +12,12 @@ public class Storage {
         fw.close();
     }
 
-    private static void appendToFile(String filePath, String textToAppend) throws IOException {
-        FileWriter fw = new FileWriter(filePath, true);
-        fw.write(textToAppend);
-        fw.close();
+    private static void appendToFile(String filePath, String textToAppend,String textToAppend2) throws IOException {
+        BufferedWriter bf = new BufferedWriter(new FileWriter(filePath));
+        bf.write(textToAppend);
+        bf.write(textToAppend2);
+        bf.newLine();
+        bf.close();
     }
 
 
