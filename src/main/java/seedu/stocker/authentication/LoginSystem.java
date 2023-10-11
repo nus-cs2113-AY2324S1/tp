f4fec5773b73e56a551fc2f99440e3b21f9646b1
TeoHaoZhi <e0957449@u.nus.edu>
2023-10-11 22:17:18 +0800

    Fix bugs in login system class

--- src/main/java/seedu/stocker/authentication/LoginSystem.java
+++ src/main/java/seedu/stocker/authentication/LoginSystem.java
@@ -1,20 +1,24 @@
 package seedu.stocker.authentication;
 
-
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class LoginSystem {
-
+    public boolean loginStatus;
     private HashMap<String, String> users;
+
+
 
     public LoginSystem() throws IOException {
         users = new HashMap<>();
+        loginStatus = false;
 
         File holder = new File("./users.txt");
         if (holder.exists() == false) {
@@ -26,11 +30,15 @@ public class LoginSystem {
         }
     }
 
-    public String showWelcomeMessage() {
+    public void showWelcomeMessage() {
+        System.out.println("Welcome! Key in the respective number 1 or 2 based on your needs \n"
+                + "1.Register user \n" + "2.Login ");
+    }
+
+    public String authenticateUserChoice() {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Welcome! Key in the respective number based on your needs \n"
-                + "1.Register user \n" + "2.Login ");
+
         String input = in.nextLine();
 
         if (input.equals("1")) {
@@ -64,12 +72,13 @@ public class LoginSystem {
         } else {
             users.put(username, password);
             System.out.println("Registration successful.");
+            loginStatus = true;
         }
         writeNewUserToFile();
 
     }
 
-    public void loginExistingUser() {
+    public void loginExistingUser() throws IOException {
 
         Scanner input = new Scanner(System.in);
 
@@ -78,12 +87,34 @@ public class LoginSystem {
         System.out.println("Enter your password:");
         String passwordInput = input.nextLine();
         if (!users.containsKey(usernameInput)) {
-            System.out.println("Invalid username or password.");
+            System.out.println("Invalid username or password. Please try again.");
+            System.out.println();
+            System.out.println("Key in the respective number 1 or 2 based on your needs \n"
+                    + "1.Register user \n" + "2.Login ");
+
+            String choice = input.nextLine();
+            if (choice.equals("1")) {
+                newUserCreator();
+
+            } else if (choice.equals("2")) {
+                loginExistingUser();
+            }
         } else {
             if (users.get(usernameInput).equals(passwordInput)) {
                 System.out.println("Login successful.");
+                loginStatus = true;
             } else {
-                System.out.println("Invalid username or password.");
+                System.out.println("Invalid username or password. Please try again");
+                System.out.println();
+                System.out.println("Key in the respective number 1 or 2 based on your needs \n"
+                        + "1.Register user \n" + "2.Login ");
+                String choice = input.nextLine();
+                if (choice.equals("1")) {
+                    newUserCreator();
+
+                } else if (choice.equals("2")) {
+                    loginExistingUser();
+                }
             }
         }
 
@@ -91,7 +122,7 @@ public class LoginSystem {
     }
 
     public void writeNewUserToFile() throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("./users.txt",true));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("./users.txt", true));
 
         for (Map.Entry<String, String> entry :
                 users.entrySet()) {
@@ -111,8 +142,25 @@ public class LoginSystem {
 
     }
 
+    public void loadExistingUsers() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("./users.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(":", 2);
+            if (parts.length >= 2) {
+                String key = parts[0];
+                String value = parts[1];
+                users.put(key, value);
+            } else {
+                System.out.println("ignoring line: " + line);
+            }
+        }
+    }
+
     public int run() throws IOException {
-        String choice = showWelcomeMessage();
+        loadExistingUsers();
+        showWelcomeMessage();
+        String choice = authenticateUserChoice();
         if (choice.equals("1")) {
             newUserCreator();
 
