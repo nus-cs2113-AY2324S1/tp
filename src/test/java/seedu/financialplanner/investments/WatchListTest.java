bef5bea11ba3f9ee327b12830092670706eb6c8c
wwweert123 <puachengxuan.fp@gmail.com>
2023-10-12 13:39:13 +0800

    Add testcases for addstock and fetchFMPStockPrices

---
+++ src/test/java/seedu/financialplanner/investments/WatchListTest.java
@@ -0,0 +1,31 @@
+package seedu.financialplanner.investments;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+import seedu.financialplanner.exceptions.FinancialPlannerException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WatchListTest {
+
+    @Test
+    void fetchFMPStockPrices() {
+        WatchList wl = new WatchList();
+        JSONArray obj = wl.fetchFMPStockPrices();
+        JSONObject apple = (JSONObject) obj.get(0);
+        assertNotNull(apple.get("price"));
+        JSONObject meta = (JSONObject) obj.get(1);
+        assertNotNull(meta.get("price"));
+        JSONObject google = (JSONObject) obj.get(2);
+        assertNotNull(google.get("price"));
+    }
+
+    @Test
+    void addStock() throws Exception {
+        WatchList wl = new WatchList();
+        String market = "NYSE";
+        String stockCode = "GME";
+        assertEquals("GameStop Corp.", wl.addStock(market, stockCode));
+    }
+}
