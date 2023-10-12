7c06aff1313bf138f10ec7375ff55889f31f8e56
lisizhuang-0121 <sizhuangli6@gmail.com>
2023-10-12 14:07:37 +0800

    Add Invalid Command Class for improved command parsing

---
+++ src/main/java/quizhub/command/CommandInvalid.java
@@ -0,0 +1,20 @@
+package quizhub.command;
+
+import quizhub.questionlist.QuestionList;
+import quizhub.storage.Storage;
+import quizhub.ui.Ui;
+
+public class CommandInvalid extends Command {
+
+    private String feedback;
+
+    public CommandInvalid(String feedback) {
+        super(CommandType.INVALID);
+        this.feedback = feedback;
+    }
+
+    @Override
+    public void executeCommand(Ui ui, Storage dataStorage, QuestionList questions) {
+        ui.showInvalidCommandHelp(feedback);
+    }
+}
