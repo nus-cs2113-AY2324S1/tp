0b368b322b9776bb5b7a48d27ef2982113f83c3f
J0shuaLeong <joshualeong2607@gmail.com>
2023-10-13 20:36:13 +0800

    Edited goodbye message

--- src/main/java/fittrack/UserProfile.java
+++ src/main/java/fittrack/UserProfile.java
@@ -5,36 +5,9 @@ public class UserProfile {
     private double height;
     private double weight;
 
-    public UserProfile() {
-    }
-
     public UserProfile(String name, double height, double weight) {
         this.name = name;
         this.height = height;
-        this.weight = weight;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
         this.weight = weight;
     }
 }
