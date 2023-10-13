adf0164a97ba9a74099f85e77e8ca9453d9aeda1
Wendelin Wemhoener <wendelin.wemhoener@gmail.com>
2023-10-13 11:42:36 +0800

    Add new testcases

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,12 +1,37 @@
 package seedu.duke;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.flashcard.FlashcardComponent;
+import seedu.duke.flashcard.command.UnknownCommand;
+
+import java.util.ArrayList;
 
 class DukeTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testFlashcardComponent_isResponsible_notResponsible() {
+        FlashcardComponent flashcardComponent = new FlashcardComponent(
+                new ArrayList<>());
+
+        assertFalse(flashcardComponent.isResponsible("dfdfdfdfdf"));
+        assertFalse(flashcardComponent.isResponsible("help me"));
+        assertFalse(flashcardComponent.isResponsible(" "));
+    }
+
+    @Test
+    public void testFlashcardComponent_isResponsible_responsible() {
+        FlashcardComponent flashcardComponent = new FlashcardComponent(
+                new ArrayList<>());
+
+        assertTrue(flashcardComponent.isResponsible("create flashcard"));
+        assertTrue(flashcardComponent.isResponsible("create flashcard  "));
+        assertTrue(flashcardComponent.isResponsible("list flashcards"));
     }
 }
