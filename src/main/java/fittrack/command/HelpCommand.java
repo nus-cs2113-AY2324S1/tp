06fe2465cbc89fa15ed477cb2452d868f0a33704
ICubE- <jehoyeon01@gmail.com>
2023-10-13 16:46:24 +0800

    Fix description and usage of help command

--- src/main/java/fittrack/command/HelpCommand.java
+++ src/main/java/fittrack/command/HelpCommand.java
@@ -7,9 +7,11 @@ import static fittrack.parser.CommandParser.ALL_COMMAND_WORDS;
 public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
-    private static final String DESCRIPTION = "`" + COMMAND_WORD + "` shows help message of the command.";
+    private static final String DESCRIPTION =
+            String.format("`%s` shows help message of the command.", COMMAND_WORD);
     private static final String KNOWN_COMMANDS = "Existing commands:\n" + ALL_COMMAND_WORDS;
-    private static final String USAGE = "Type `help` or `help <COMMAND>` to view help.";
+    private static final String USAGE =
+            String.format("Type `%s` or `%s <COMMAND>` to view help.", COMMAND_WORD, COMMAND_WORD);
     private static final String HELP = DESCRIPTION + "\n" + KNOWN_COMMANDS + "\n" + USAGE;
 
     private static final String MESSAGE_INVALID_COMMAND = "`%s` is an invalid command.\n" + USAGE;
