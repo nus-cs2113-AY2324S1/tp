7ed9cafa3c7bd26a4641c1ec66c018e371c42434
YFshadaow <renzd2001@gmail.com>
2023-10-16 00:05:23 +0800

    Update code to pass style checks

--- src/main/java/seedu/financialplanner/utils/Parser.java
+++ src/main/java/seedu/financialplanner/utils/Parser.java
@@ -1,8 +1,23 @@
 package seedu.financialplanner.utils;
 
-import seedu.financialplanner.commands.*;
 
-import java.util.*;
+
+import seedu.financialplanner.commands.AbstractCommand;
+import seedu.financialplanner.commands.AddStockCommand;
+import seedu.financialplanner.commands.EntryCommand;
+import seedu.financialplanner.commands.ExitCommand;
+import seedu.financialplanner.commands.FindCommand;
+import seedu.financialplanner.commands.InvalidCommand;
+import seedu.financialplanner.commands.RawCommand;
+import seedu.financialplanner.commands.WatchListCommand;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 
 public class Parser {
     private static final String EXIT_COMMAND_NAME = "exit";
@@ -16,6 +31,28 @@ public class Parser {
         return parseCommand(rawCommand);
     }
 
+    public static AbstractCommand parseCommand(RawCommand rawCommand) throws IllegalArgumentException{
+        switch (rawCommand.getCommandName()) {
+        case EXIT_COMMAND_NAME: {
+            return new ExitCommand(rawCommand);
+        }
+        case WATCHLIST_COMMAND_NAME: {
+            return new WatchListCommand(rawCommand);
+        }
+        case ADD_ENTRY_COMMAND_NAME: {
+            return new EntryCommand(rawCommand);
+        }
+        case ADD_STOCK_COMMAND_NAME: {
+            return new AddStockCommand(rawCommand);
+        }
+        case FIND_COMMAND_NAME: {
+            return new FindCommand(rawCommand);
+        }
+        default: {
+            return new InvalidCommand();
+        }
+        }
+    }
     public static RawCommand parseRawCommand(String input) throws IllegalArgumentException{
         Iterator<String> iterator = Arrays.stream(input.split(" ")).iterator();
         if (!iterator.hasNext()) {
@@ -34,7 +71,8 @@ public class Parser {
                 // Save previous extra argument when next extra argument is found
                 if (currentExtraArgumentName != null) {
                     if (extraArgs.containsKey(currentExtraArgumentName)) {
-                        throw new IllegalArgumentException(String.format("Duplicate extra argument name: %s", currentExtraArgumentName));
+                        throw new IllegalArgumentException(
+                                String.format("Duplicate extra argument name: %s", currentExtraArgumentName));
                     } else {
                         extraArgs.put(currentExtraArgumentName, String.join(" ", extraArgumentContentBuffer));
                         extraArgumentContentBuffer.clear();
@@ -58,7 +96,8 @@ public class Parser {
         // Save previous extra argument at the very end
         if (currentExtraArgumentName != null) {
             if (extraArgs.containsKey(currentExtraArgumentName)) {
-                throw new IllegalArgumentException(String.format("Duplicate extra argument name: %s", currentExtraArgumentName));
+                throw new IllegalArgumentException(
+                        String.format("Duplicate extra argument name: %s", currentExtraArgumentName));
             } else {
                 extraArgs.put(currentExtraArgumentName, String.join(" ", extraArgumentContentBuffer));
                 extraArgumentContentBuffer.clear();
@@ -68,26 +107,4 @@ public class Parser {
         return new RawCommand(commandName, args, extraArgs);
     }
 
-    public static AbstractCommand parseCommand(RawCommand rawCommand) throws IllegalArgumentException{
-        switch (rawCommand.getCommandName()) {
-            case EXIT_COMMAND_NAME: {
-                return new ExitCommand(rawCommand);
-            }
-            case WATCHLIST_COMMAND_NAME: {
-                return new WatchListCommand(rawCommand);
-            }
-            case ADD_ENTRY_COMMAND_NAME: {
-                return new EntryCommand(rawCommand);
-            }
-            case ADD_STOCK_COMMAND_NAME: {
-                return new AddStockCommand(rawCommand);
-            }
-            case FIND_COMMAND_NAME: {
-                return new FindCommand(rawCommand);
-            }
-            default: {
-                return new InvalidCommand();
-            }
-        }
-    }
 }
