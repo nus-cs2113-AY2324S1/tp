68fc9ae55171009c217fc93e846fa88e4c0b913a
wwweert123 <puachengxuan.fp@gmail.com>
2023-10-16 14:44:34 +0800

    Fix identation in visualizer

--- src/main/java/seedu/financialplanner/visualisations/Visualizer.java
+++ src/main/java/seedu/financialplanner/visualisations/Visualizer.java
@@ -36,11 +36,11 @@ public class Visualizer {
 
         // Customize Chart
         Color[] sliceColors = new Color[] {
-                new Color(224, 68, 14),
-                new Color(230, 105, 62),
-                new Color(236, 143, 110),
-                new Color(243, 180, 159),
-                new Color(246, 199, 182)
+            new Color(224, 68, 14),
+            new Color(230, 105, 62),
+            new Color(236, 143, 110),
+            new Color(243, 180, 159),
+            new Color(246, 199, 182)
         };
         chart.getStyler().setSeriesColors(sliceColors);
 
