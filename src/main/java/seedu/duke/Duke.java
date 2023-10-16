ec3ad41cac129fdce30e4415477e1dded5257571
Cazh1 <cazh1or2@gmail.com>
2023-10-17 03:05:40 +0800

    Update Duke.java

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,36 +1,18 @@
 package seedu.duke;
 
-import seedu.duke.command.Command;
-import seedu.duke.data.Menu;
-import seedu.duke.parser.Parser;
-import seedu.duke.ui.Ui;
-import seedu.duke.command.ExitCommand;
+import java.util.Scanner;
 
 public class Duke {
-    /**
-     * Main entry-point for the java.duke.Duke application.
-     */
-    private Ui ui;
-    private Menu menu;
     public static void main(String[] args) {
-        new Duke().run();
-    }
-
-    /** Runs the program until termination.  */
-    public void run() {
-        runCommandLoopUntilExitCommand();
-        exit(ui);
-    }
-    /** Reads the user command and executes it, until the user issues the exit command.  */
-    public void runCommandLoopUntilExitCommand() {
-        do {
-            String item = ui.receiveUserInput();
-            Command c = Parser.parseCommand(menu, item);
-            c.execute(menu, ui);
-        } while (!ExitCommand.isExit());
-    }
-    private static void exit(Ui ui) {
-        ui.showGoodbye();
-        System.exit(0);
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What is your name?");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
     }
 }
+
