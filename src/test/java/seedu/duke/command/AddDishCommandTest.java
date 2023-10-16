69b789b05598d52130d17ad17ceeec896c42ef5e
Dexter Hoon <e0727166@u.nus.edu>
2023-10-17 02:38:26 +0800

    Add testing for add dish command
    Fixes #48

--- src/test/java/seedu/duke/command/AddDishCommandTest.java
+++ src/test/java/seedu/duke/command/AddDishCommandTest.java
@@ -1,5 +1,29 @@
 package seedu.duke.command;
 
-class AddDishCommandTest {
+import org.junit.jupiter.api.Test;
+import seedu.duke.data.Menu;
+import seedu.duke.data.dish.Dish;
+import seedu.duke.data.dish.Ingredient;
+import seedu.duke.ui.Ui;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddDishCommandTest {
+    @Test
+    void execute_dishAdded_expectDishInMenu() {
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        ArrayList<Dish> menuItems = new ArrayList<>();
+        Menu menu = new Menu(menuItems);
+        Ui ui = new Ui();
+        //adding a dish to the menu
+        ingredients.add(new Ingredient("chicken", "100g"));
+        Dish dish = new Dish("Chicken Rice", ingredients, (float) 1.00);
+        AddDishCommand addDishCommand = new AddDishCommand(dish);
+
+        addDishCommand.execute(menu, ui);
+
+        assertEquals(1, menu.getMenuItemsList().size());
+    }
 }
