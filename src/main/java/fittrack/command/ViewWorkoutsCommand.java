ae01b88887c2b68e3826bf0740fd5198d8d5617a
Faris Sirraj <faris@curium.sg>
2023-10-17 01:11:27 +0800

    Add help messages for the ViewWorkoutsCommand

--- src/main/java/fittrack/command/ViewWorkoutsCommand.java
+++ src/main/java/fittrack/command/ViewWorkoutsCommand.java
@@ -3,8 +3,17 @@ package fittrack.command;
 import fittrack.parser.CommandParser;
 
 public class ViewWorkoutsCommand extends Command {
-    public static final String COMMAND_WORD = "viewworkouts";
+    public static final String COMMAND_WORD = "viewWorkouts";
+    private static final String DESCRIPTION =
+            String.format("'%s' shows the list of all workouts.", COMMAND_WORD);
+    private static final String USAGE =
+            String.format("Type '%s' to view the list of your workouts.", COMMAND_WORD);
+    private static final String HELP = DESCRIPTION + "\n" + USAGE;
 
+    /**
+     * Execute the command
+     * @return list of workouts
+     */
     @Override
     public CommandResult execute() {
         return null;
