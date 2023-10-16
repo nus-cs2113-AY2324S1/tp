8e4fb479ae61dfc19385dee50a5acffe9072dd7d
Dexter Hoon <e0727166@u.nus.edu>
2023-10-16 22:36:13 +0800

    Add commonly used methods

--- src/main/java/seedu/duke/data/Menu.java
+++ src/main/java/seedu/duke/data/Menu.java
@@ -10,4 +10,16 @@ public class Menu {
     public Menu(ArrayList<Dish> menuItems) {
         this.menuItems = menuItems;
     }
+    public int getSize() {
+        return menuItems.size();
+    }
+    public Dish getDish(int menuID) {
+        return menuItems.get(menuID);
+    }
+    public void remove(int menuID) {
+        menuItems.remove(menuID);
+    }
+    public void add(Dish dish) {
+        menuItems.add(dish);
+    }
 }
--- src/main/java/seedu/duke/data/dish/Ingredient.java
+++ src/main/java/seedu/duke/data/dish/Ingredient.java
@@ -2,8 +2,20 @@ package seedu.duke.data.dish;
 
 public class Ingredient {
     private final String name;
+    private final String quantity;
 
     public Ingredient(String name) {
         this.name = name;
+        quantity = null;
+    }
+
+    public Ingredient(String name, String quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + quantity;
     }
 }
