c32e4c62168dba8284fb862f1bdcebd8ceaad008
NeoMinWei <>
2023-10-16 23:34:42 +0800

    Add error handling for empty type string

--- src/main/java/seedu/financialplanner/commands/AddCashflowCommand.java
+++ src/main/java/seedu/financialplanner/commands/AddCashflowCommand.java
@@ -6,7 +6,6 @@ import seedu.financialplanner.list.Cashflow;
 import seedu.financialplanner.list.CashflowList;
 import seedu.financialplanner.utils.Ui;
 
-import java.awt.geom.Arc2D;
 import java.util.ArrayList;
 import static java.lang.Math.abs;
 
@@ -14,7 +13,7 @@ public class AddCashflowCommand extends AbstractCommand {
 
     protected double amount;
     protected CashflowCategory category;
-    protected String type;
+    protected String type = null;
     protected int recur = 0;
 
     public AddCashflowCommand(RawCommand rawCommand) throws IllegalArgumentException {
@@ -33,12 +32,18 @@ public class AddCashflowCommand extends AbstractCommand {
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("Amount must be a number");
         }
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount cannot be negative");
+        }
         rawCommand.extraArgs.remove("a");
 
         if (!rawCommand.extraArgs.containsKey("t")) {
             throw new IllegalArgumentException("Entry must have a type");
         }
         type = rawCommand.extraArgs.get("t");
+        if (type.isBlank()) {
+            throw new IllegalArgumentException("Type cannot be left empty");
+        }
         rawCommand.extraArgs.remove("t");
 
         if (rawCommand.extraArgs.containsKey("r")) {
@@ -48,6 +53,9 @@ public class AddCashflowCommand extends AbstractCommand {
                 throw new IllegalArgumentException("Recurrence must be an integer");
             }
             rawCommand.extraArgs.remove("r");
+        }
+        if (recur < 0) {
+            throw new IllegalArgumentException("Amount cannot be negative");
         }
         if (!rawCommand.extraArgs.isEmpty()) {
             String unknownExtraArgument = new ArrayList<>(rawCommand.extraArgs.keySet()).get(0);
@@ -60,6 +68,7 @@ public class AddCashflowCommand extends AbstractCommand {
         assert category.equals(CashflowCategory.INCOME) || category.equals(CashflowCategory.EXPENSE);
         assert recur >= 0;
         assert amount >= 0;
+        assert type != null;
 
         switch (category) {
         case INCOME:
