ac4e57130e2e050f03f1ccd7411de2e88ee69082
Cazh1 <cazh1or2@gmail.com>
2023-10-17 02:17:29 +0800

    Update ListMenuCommand.java

--- src/main/java/seedu/duke/command/ListMenuCommand.java
+++ src/main/java/seedu/duke/command/ListMenuCommand.java
@@ -4,10 +4,22 @@ import seedu.duke.data.Menu;
 //import seedu.duke.data.dish.Dish;
 import seedu.duke.ui.Ui;
 
+import java.text.DecimalFormat;
+
 /**
  * Lists all dishes in the menu to the user.
  */
 public class ListMenuCommand extends Command {
+
+    public static final String COMMAND_WORD = "list_menu";
+    private static final DecimalFormat dollarValue = new DecimalFormat("0.00");
+    /**
+     * Iterates through the menu arraylist, outputting the dish name and dish price.
+     *
+     * @param menu ArrayList of Dishes
+     * @param ui Handles the interactions with user
+     */
+
     @Override
     public void execute(Menu menu, Ui ui) {
         /*for (String d : menu) {
