b67df48ceadf6e7b7d4972f4ab094428396d9293
ziyi105 <ahziyi@gmail.com>
2023-10-17 00:30:07 +0800

    Create EditPriceCommand.java

---
+++ src/main/java/seedu/duke/command/EditPriceCommand.java
@@ -0,0 +1,21 @@
+package seedu.duke.command;
+
+import seedu.duke.data.Menu;
+import seedu.duke.ui.Ui;
+
+/**
+ * Edit the price of a dish of a certain index
+ */
+public class EditPriceCommand extends Command {
+    private final int menuID;
+    private final float newPrice;
+
+    public EditPriceCommand(int menuID, float newPrice) {
+        this.menuID = menuID;
+        this.newPrice = newPrice;
+    }
+
+    public void execute(Menu menu, Ui ui) {
+        menu.getDish(this.menuID).setPrice(this.newPrice);
+    }
+}
