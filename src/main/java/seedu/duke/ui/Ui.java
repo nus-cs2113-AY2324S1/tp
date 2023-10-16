74f4f4d8f197fab8035bc1e7a855ccbd426dfc11
Dexter Hoon <e0727166@u.nus.edu>
2023-10-17 00:34:52 +0800

    Implement add dish feature

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -1,8 +1,12 @@
 package seedu.duke.ui;
 
+import seedu.duke.data.dish.Dish;
+import seedu.duke.data.dish.Ingredient;
+
 import java.util.Scanner;
 
 public class Ui {
+    public static final int OFFSET_LIST_INDEX = 1;
     private final Scanner scanner;
 
     /**
@@ -23,7 +27,32 @@ public class Ui {
     public void showGoodbye() {
         System.out.println(UserOutput.GOODBYE_MESSAGE.message);
     }
-    /** Shows messages(s) to the user **/
+
+    public void printAddDishMessage(Dish dish) {
+        String dishNameString = "Dish Name: " + dish.getName();
+        String dishPriceString = "Dish Price: $" + dish.getPrice();
+        StringBuilder dishIngredientsString = new StringBuilder("Ingredients:\n");
+
+        for (int i = 0; i < dish.getIngredients().size(); i++) {
+            Ingredient ingredient = dish.getIngredients().get(i);
+
+            dishIngredientsString.append("\t")
+                    .append(i + OFFSET_LIST_INDEX)
+                    .append(". ")
+                    .append(ingredient.toString())
+                    .append("\n");
+        }
+
+        showToUser(UserOutput.ADD_DISH_MESSAGE.message,
+                dishNameString,
+                dishPriceString,
+                dishIngredientsString.toString());
+    }
+
+    /**
+     * Shows messages(s) to the user
+     * @param message string(s) of messages to print
+     */
     public void showToUser(String... message) {
         for (String m: message) {
             System.out.println(m);
