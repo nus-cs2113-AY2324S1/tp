b8a856dcc26db8ede044ef2bcac0852bcdb3838e
Bryan Lee <e0725090@u.nus.edu>
2023-10-16 14:45:16 +0800

    no message

---
+++ src/main/java/seedu/duke/commands/KaChinnnngException.java
@@ -0,0 +1,10 @@
+package seedu.duke.commands;
+
+/**
+ * Represents the exception thrown when there is an error in the command.
+ */
+public class KaChinnnngException extends Exception{
+    public KaChinnnngException(String message) {
+        super(message);
+    }
+}
