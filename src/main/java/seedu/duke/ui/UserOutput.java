67f65e2b750751127920b80b45d162c28849b99f
Cazh1 <cazh1or2@gmail.com>
2023-10-16 22:17:06 +0800

    Update Menu.java methods

--- src/main/java/seedu/duke/ui/UserOutput.java
+++ src/main/java/seedu/duke/ui/UserOutput.java
@@ -5,7 +5,8 @@ package seedu.duke.ui;
  */
 public enum UserOutput {
     WELCOME_MESSAGE("Hello! Welcome to CafeCTRL!"),
-    GOODBYE_MESSAGE("Goodbye <3 Have a great day ahead!");
+    GOODBYE_MESSAGE("Goodbye <3 Have a great day ahead!"),
+    LIST_MESSAGE("Ah, behold, the grand menu of delights! 🍽️");
 
     public final String message;
     UserOutput(String message) {
