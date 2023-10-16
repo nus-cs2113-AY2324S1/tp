71629404bad7b6184f37a4cff09948061ab74dda
Cazh1 <cazh1or2@gmail.com>
2023-10-17 03:00:43 +0800

    Update ListMenuCommandTest.java

--- src/test/java/seedu/duke/command/ListMenuCommandTest.java
+++ src/test/java/seedu/duke/command/ListMenuCommandTest.java
@@ -9,13 +9,13 @@ import java.util.ArrayList;
 
 class ListMenuCommandTest {
     @Test
-    public static void execute_outputTwoDishes_() {
+    public static void execute_expectTwoDishes() {
         ArrayList<Dish> menuItems = new ArrayList<>();
         menuItems.add(new Dish("Chicken Rice", 2.50F));
         menuItems.add(new Dish("Chicken Curry", 4.30F));
         Menu menu = new Menu(menuItems);
 
-        Command ListMenuCommand = new ListMenuCommand();
-        ListMenuCommand.execute(menu, new Ui());
+        Command listMenuCommand = new ListMenuCommand();
+        listMenuCommand.execute(menu, new Ui());
     }
 }
