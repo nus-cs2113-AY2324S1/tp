ad6b223efae6bf4be468bf1ad7cbeb772c58d774
Cazh1 <cazh1or2@gmail.com>
2023-10-17 02:14:22 +0800

    Implement Junit for ListCommand.java

--- src/test/java/seedu/duke/command/ListMenuCommandTest.java
+++ src/test/java/seedu/duke/command/ListMenuCommandTest.java
@@ -1,5 +1,21 @@
 package seedu.duke.command;
 
-class ListMenuCommandTest {
+import org.junit.jupiter.api.Test;
+import seedu.duke.data.Menu;
+import seedu.duke.data.dish.Dish;
+import seedu.duke.ui.Ui;
 
+import java.util.ArrayList;
+
+class ListMenuCommandTest {
+    @Test
+    public static void main(String[] args) {
+        ArrayList<Dish> menuItems = new ArrayList<>();
+        menuItems.add(new Dish("Chicken Rice", 2.50F));
+        menuItems.add(new Dish("Chicken Curry", 4.30F));
+        Menu menu = new Menu(menuItems);
+
+        Command ListMenuCommand = new ListMenuCommand();
+        ListMenuCommand.execute(menu, new Ui());
+    }
 }
