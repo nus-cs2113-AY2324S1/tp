38db96ca3ba4455bd7d7ed7bbea54e9450a1d4d2
Ethan <ethanhuang03@gmail.com>
2023-10-16 10:06:43 +0800

    Add execute method to LogCommand

--- src/main/java/seedu/duke/commands/LogCommand.java
+++ src/main/java/seedu/duke/commands/LogCommand.java
@@ -1,4 +1,26 @@
 package seedu.duke.commands;
 
-public class LogCommand {
+import java.util.List;
+
+import seedu.duke.exerciselog.Log;
+
+public class LogCommand extends Command {
+
+    public String feedbackToUser;
+
+    public LogCommand() {
+        super();
+        feedbackToUser = "Successfully added exercise to log!";
+    }
+
+    public CommandResult execute(Log exerciseLog, List<String> exerciseDetails) {
+        int month = Integer.parseInt(exerciseDetails.get(0));
+        int day = Integer.parseInt(exerciseDetails.get(1));
+        String exerciseName = exerciseDetails.get(2);
+        int caloriesBurned = Integer.parseInt(exerciseDetails.get(3));
+
+        exerciseLog.addExercise(month, day, exerciseName, caloriesBurned);
+
+        return new CommandResult(feedbackToUser);
+    }
 }
