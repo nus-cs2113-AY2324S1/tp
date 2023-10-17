5c4f2a2d9bfa034d6d40cd0afa40053b009f00d0
ICubE- <jehoyeon01@gmail.com>
2023-10-17 17:38:07 +0800

    Change HELPs' access modifier to public

--- src/main/java/fittrack/command/HelpCommand.java
+++ src/main/java/fittrack/command/HelpCommand.java
@@ -12,9 +12,9 @@ public class HelpCommand extends Command {
     private static final String KNOWN_COMMANDS = "Existing commands:\n" + ALL_COMMAND_WORDS;
     private static final String USAGE =
             String.format("Type `%s` or `%s <COMMAND>` to view help.", COMMAND_WORD, COMMAND_WORD);
-    private static final String HELP = DESCRIPTION + "\n" + KNOWN_COMMANDS + "\n" + USAGE;
+    public static final String HELP = DESCRIPTION + "\n" + KNOWN_COMMANDS + "\n" + USAGE;
 
-    private static final String MESSAGE_INVALID_COMMAND = "`%s` is an invalid command.\n" + USAGE;
+    public static final String MESSAGE_INVALID_COMMAND = "`%s` is an invalid command.\n" + USAGE;
 
     private String helpMessage;
 
