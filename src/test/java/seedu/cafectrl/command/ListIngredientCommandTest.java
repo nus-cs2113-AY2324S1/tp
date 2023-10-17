421167b78f8a3be9664ab6d40146164793398dc6
NaychiMin <naychimin02@gmail.com>
2023-10-18 01:57:50 +0800

    Improve coding standard.
    Fixes #56

--- src/test/java/seedu/cafectrl/command/ListIngredientCommandTest.java
+++ src/test/java/seedu/cafectrl/command/ListIngredientCommandTest.java
@@ -8,7 +8,6 @@ import seedu.cafectrl.ui.Ui;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -17,11 +16,15 @@ class ListIngredientCommandTest {
     @Test
     public void execute_validIndex_printsIngredients() {
         ArrayList<Dish> menuItems = new ArrayList<>();
-        menuItems.add(new Dish("Chicken Rice", new ArrayList<>(Arrays.asList(new Ingredient("Rice", "1 cup"), new Ingredient("Chicken", "100g"))), 8.0F));
-        menuItems.add(new Dish("Chicken Sandwich", new ArrayList<>(Arrays.asList(new Ingredient("Lettuce", "100g"), new Ingredient("Chicken", "50g"))), 5.0F));
+        menuItems.add(new Dish("Chicken Rice",
+                new ArrayList<>(Arrays.asList(new Ingredient("Rice", "1 cup"),
+                        new Ingredient("Chicken", "100g"))), 8.0F));
+        menuItems.add(new Dish("Chicken Sandwich",
+                new ArrayList<>(Arrays.asList(new Ingredient("Lettuce", "100g"),
+                        new Ingredient("Chicken", "50g"))), 5.0F));
         Menu menu = new Menu(menuItems);
 
-        List<String> actualOutput = new ArrayList<>();
+        ArrayList<String> actualOutput = new ArrayList<>();
         Ui ui = new Ui() {
             @Override
             public void showToUser(String... message) {
@@ -37,13 +40,18 @@ class ListIngredientCommandTest {
                 "Rice - 1 cup\n" +
                 "Chicken - 100g\n";
 
-        assertEquals(expectedOutput.trim().replaceAll("\\s+", " "), actualOutput.get(0).trim().replaceAll("\\s+", " "));
+        assertEquals(expectedOutput.trim().replaceAll("\\s+", " "),
+                actualOutput.get(0).trim().replaceAll("\\s+", " "));
     }
 
     public void execute_invalidIndex_returnsErrorMessage() {
         ArrayList<Dish> menuItems = new ArrayList<>();
-        menuItems.add(new Dish("Chicken Rice", new ArrayList<>(Arrays.asList(new Ingredient("Rice", "1 cup"), new Ingredient("Chicken", "100g"))), 8.0F));
-        menuItems.add(new Dish("Chicken Sandwich", new ArrayList<>(Arrays.asList(new Ingredient("Lettuce", "100g"), new Ingredient("Chicken", "50g"))), 5.0F));
+        menuItems.add(new Dish("Chicken Rice",
+                new ArrayList<>(Arrays.asList(new Ingredient("Rice", "1 cup"),
+                        new Ingredient("Chicken", "100g"))), 8.0F));
+        menuItems.add(new Dish("Chicken Sandwich",
+                new ArrayList<>(Arrays.asList(new Ingredient("Lettuce", "100g"),
+                        new Ingredient("Chicken", "50g"))), 5.0F));
         Menu menu = new Menu(menuItems);
         Ui ui = new Ui();
         int invalidIndex = 3;
