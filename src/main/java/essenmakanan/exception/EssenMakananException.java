3aa7bf85c7604d1b0927edad2f87672f09c841f7
charkty <charkty@gmail.com>
2023-10-17 17:09:21 +0800

    Remove unused imports

--- src/main/java/essenmakanan/exception/EssenMakananException.java
+++ src/main/java/essenmakanan/exception/EssenMakananException.java
@@ -1,7 +1,5 @@
 package essenmakanan.exception;
 
-import essenmakanan.EssenMakanan;
-
 public class EssenMakananException extends Exception {
     public EssenMakananException(String message) {
         super("EssenMakanan Exception! " + message);
--- src/test/java/essenmakanan/IngredientListTest.java
+++ src/test/java/essenmakanan/IngredientListTest.java
@@ -1,12 +1,9 @@
 package essenmakanan;
 import essenmakanan.ingredient.Ingredient;
 import essenmakanan.ingredient.IngredientList;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 public class IngredientListTest {
 
