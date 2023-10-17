9ee9475bcea3849656b21a218ce39af821019a92
charkty <charkty@gmail.com>
2023-10-17 15:02:41 +0800

    Add Ingredient and IngredientList class

--- src/main/java/essenmakanan/ingredient/Ingredient.java
+++ src/main/java/essenmakanan/ingredient/Ingredient.java
@@ -1,4 +1,28 @@
 package essenmakanan.ingredient;
 
 public class Ingredient {
+    private String name;
+    private String qty;
+    public Ingredient(String name) {
+        this.name = name;
+    }
+    public Ingredient(String name, String qty) {
+        this.name = name;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQty() {
+        return this.qty;
+    }
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
 }
--- src/main/java/essenmakanan/ingredient/IngredientList.java
+++ src/main/java/essenmakanan/ingredient/IngredientList.java
@@ -1,4 +1,29 @@
 package essenmakanan.ingredient;
+import java.util.ArrayList;
 
 public class IngredientList {
+    private ArrayList<Ingredient> ingredients;
+
+    public IngredientList() {
+        ingredients = new ArrayList<>();
+    }
+
+    public void addIngredient(String ingredientName) {
+        ingredients.add(new Ingredient(ingredientName));
+    }
+
+    public void deleteIngredient(Ingredient ingredient) {
+        ingredients.remove(ingredient);
+    }
+
+    public void listIngredients() {
+        int ingredientCount = 0;
+        String ingredientName;
+
+        for (int i = 0; i < ingredients.size(); i++){
+            ingredientCount++;
+            ingredientName = ingredients.get(i).getName();
+            System.out.println(ingredientCount + ". " + ingredientName);
+        }
+    }
 }
