d0887fddfc277f95e1bf8f4bb87e21dbb42207d3
danielpappa <daniel.pappalardo@icloud.com>
2023-10-17 18:16:26 +0800

    Add test for getDate of income

--- src/test/java/cashleh/IncomeTest.java
+++ src/test/java/cashleh/IncomeTest.java
@@ -12,13 +12,22 @@ class IncomeTest {
 
     @Test
     void isOneTimeIncome() {
+        System.out.println(testIncome.getTotalIncome());
         assertEquals(testIncome.isOneTimeIncome(), "One time");
         testIncome.setOneTimeIncome(false);
         assertEquals(testIncome.isOneTimeIncome(), "Recurring");
+        System.out.println(testIncome.getTotalIncome());
     }
 
     @Test
     void testToString() {
+        System.out.println(testIncome.getTotalIncome());
         assertEquals(testIncome.toString(), "\t+ S$ 200.0: pocket money (date: 2023-10-10, One time)");
+        System.out.println(testIncome.getTotalIncome());
+    }
+
+    @Test
+    void getDate() {
+        assertEquals(testIncome.getDate(), LocalDate.of(2023, 10, 10));
     }
 }
