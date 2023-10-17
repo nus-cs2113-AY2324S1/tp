d2d4c2442c60bc271a2448e5e30c354de723dfc5
spinoandraptos <spinoandraptos@gmail.com>
2023-10-17 18:13:05 +0800

    Added tests for edit and list commands

---
+++ src/test/java/quizhub/command/CommandEditTest.java
@@ -0,0 +1,118 @@
+package quizhub.command;
+
+import org.junit.jupiter.api.*;
+
+import quizhub.question.Question;
+import quizhub.questionlist.QuestionList;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class CommandEditTest {
+    private static QuestionList questionList;
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    /**
+     * Create a new question list and populate with dummy SHORTANSWER questions
+     * */
+    @BeforeAll
+    public static void setQuestionList(){
+        questionList = new QuestionList();
+        String[] questionsToAdd = {"short Question1 / Answer1", "short Question2 / Answer2",
+                "short Question3 / Answer3", "short Question4 / Answer4"};
+        Question.qnType qnType = Question.qnType.SHORTANSWER;
+        boolean showMessage = false;
+        for (String question:questionsToAdd) {
+            questionList.addToQuestionList(question, qnType, showMessage);
+        }
+        questionList.markQuestionAsDone(1, showMessage);
+        questionList.markQuestionAsDone(3, showMessage);
+    }
+    @BeforeEach
+    public void setUpOutput() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    /**
+     * Compares system standard output with expected output
+     * Assertion error is thrown if the two are not the same
+     * */
+    private void testCliOutputCorrectness(String expectedOutput){
+        String actualOutput = outputStreamCaptor.toString().trim();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    /**
+     * Test editing with out-of-bound index
+     * */
+    @Test
+    void testEditOutOfBoundIndex(){
+        String expectedOutput = "Ono! Please enter valid question number *sobs*";
+        questionList.editQuestionByIndex(-1, "", "");
+        testCliOutputCorrectness(expectedOutput);
+    }
+
+    /**
+     * Test editing with non-integer index
+     * */
+    @Test
+    void testEditNonIntIndex(){
+        String expectedOutput = "Ono! Please enter valid question number *sobs*";
+        questionList.editQuestionByIndex('a', "", "");
+        testCliOutputCorrectness(expectedOutput);
+    }
+
+    /**
+     * Test editing with blank description and answer
+     * */
+    @Test
+    void testEditBlankDescAns(){
+        String expectedOutput = "Roger that! I have edited the following question >w< !\r\n" +
+                "        [S][X] Question1 / Answer1\n" +
+                "    Now you have 4 questions in the list! UWU";
+        questionList.editQuestionByIndex(1, "", "");
+        testCliOutputCorrectness(expectedOutput);
+    }
+    /**
+     * Test editing with filled answer and blank description
+     * */
+    @Test
+    void testEditOnlyBlankDesc(){
+        String expectedOutput = "Roger that! I have edited the following question >w< !\r\n" +
+                "        [S][] Question2 / NewAnswer\n" +
+                "    Now you have 4 questions in the list! UWU";
+        questionList.editQuestionByIndex(2, "", "NewAnswer");
+        testCliOutputCorrectness(expectedOutput);
+    }
+
+    /**
+     * Test editing with filled description and blank answer
+     * */
+    @Test
+    void testEditOnlyBlankAns(){
+        String expectedOutput = "Roger that! I have edited the following question >w< !\r\n" +
+                "        [S][X] NewDescription / Answer3\n" +
+                "    Now you have 4 questions in the list! UWU";
+        questionList.editQuestionByIndex(3, "NewDescription", "");
+        testCliOutputCorrectness(expectedOutput);
+    }
+
+    /**
+     * Test editing with filled description and answer
+     * */
+    @Test
+    void testEditNonBlankDescAns(){
+        String expectedOutput = "Roger that! I have edited the following question >w< !\r\n" +
+                "        [S][] NewDescription / NewAnswer\n" +
+                "    Now you have 4 questions in the list! UWU";
+        questionList.editQuestionByIndex(4, "NewDescription", "NewAnswer");
+        testCliOutputCorrectness(expectedOutput);
+    }
+
+}
---
+++ src/test/java/quizhub/command/CommandListTest.java
@@ -0,0 +1,105 @@
+package quizhub.command;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import quizhub.question.Question;
+import quizhub.questionlist.QuestionList;
+
+public class CommandListTest {
+    private QuestionList questionList;
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        questionList = new QuestionList();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    /**
+     * Compares system standard output with expected output
+     * Assertion error is thrown if the two are not the same
+     * */
+    private void testCliOutputCorrectness(String expectedOutput){
+        String actualOutput = outputStreamCaptor.toString().trim();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    /**
+     * Test display of empty question list
+     * */
+    @Test
+    void testListEmptyList(){
+        String expectedOutput = "No questions found! Time to add some OWO";
+        questionList.printQuestionList();
+        testCliOutputCorrectness(expectedOutput);
+    }
+    /**
+     * Test display of populated question list
+     * with one unmarked SHORTANSWER question
+     * */
+    @Test
+    void testListOneUnmarkedShort(){
+        String questionToAdd = "short Question / Answer";
+        Question.qnType qnType = Question.qnType.SHORTANSWER;
+        boolean showMessage = false;
+        questionList.addToQuestionList(questionToAdd, qnType, showMessage);
+        String expectedOutput = "1: [S][] Question / Answer";
+        questionList.printQuestionList();
+        String actualOutput = outputStreamCaptor.toString().trim();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    /**
+     * Test display of populated question list
+     * with one marked SHORTANSWER question
+     * */
+    @Test
+    void testListOneMarkedShort(){
+        String questionToAdd = "short Question / Answer";
+        Question.qnType qnType = Question.qnType.SHORTANSWER;
+        boolean showMessage = false;
+        questionList.addToQuestionList(questionToAdd, qnType, showMessage);
+        questionList.markQuestionAsDone(1, showMessage);
+        String expectedOutput = "1: [S][X] Question / Answer";
+        questionList.printQuestionList();
+        String actualOutput = outputStreamCaptor.toString().trim();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    /**
+     * Test display of populated question list
+     * with a mix of marked and unmarked SHORTANSWER questions
+     * */
+    @Test
+    void testListMixedShorts(){
+        String[] questionsToAdd = {"short Question1 / Answer1", "short Question2 / Answer2",
+                "short Question3 / Answer3", "short Question4 / Answer4"};
+        Question.qnType qnType = Question.qnType.SHORTANSWER;
+        boolean showMessage = false;
+        for (String question:questionsToAdd) {
+            questionList.addToQuestionList(question, qnType, showMessage);
+        }
+        questionList.markQuestionAsDone(1, showMessage);
+        questionList.markQuestionAsDone(3, showMessage);
+        String expectedOutput =
+                "1: [S][X] Question1 / Answer1\n" +
+                "    2: [S][] Question2 / Answer2\n" +
+                "    3: [S][X] Question3 / Answer3\n" +
+                "    4: [S][] Question4 / Answer4" ;
+        questionList.printQuestionList();
+        String actualOutput = outputStreamCaptor.toString().trim();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+}
