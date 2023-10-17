34664cd62ac912690adccfca896a2f4c5806f2d5
Ethan <ethanhuang03@gmail.com>
2023-10-17 21:21:35 +0800

    Remove redundant import in ExerciseTest

--- src/test/java/seedu/duke/exerciselog/ExerciseTest.java
+++ src/test/java/seedu/duke/exerciselog/ExerciseTest.java
@@ -1,7 +1,5 @@
 package seedu.duke.exerciselog;
 
-import seedu.duke.exerciselog.Exercise;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
