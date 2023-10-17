20c3a7e7937dad5750f2d0f4d3003a9ae922e08e
Cheezeblokz <jingxi2001@gmail.com>
2023-10-17 19:42:38 +0800

    Added basic Event object capabilities for adding, deleting, printing and access

--- src/main/java/seedu/duke/calendar/Event.java
+++ src/main/java/seedu/duke/calendar/Event.java
@@ -1,4 +1,49 @@
 package seedu.duke.calendar;
 
+import java.time.LocalDate;
+
 public class Event {
+    String name;
+    LocalDate from;
+    LocalDate to;
+
+    public Event(String name, LocalDate from, LocalDate to) {
+        this.name = name;
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getFrom() {
+        return from;
+    }
+
+    public void setFrom(LocalDate from) {
+        this.from = from;
+    }
+
+    public LocalDate getTo() {
+        return to;
+    }
+
+    public void setTo(LocalDate to) {
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "name='" + name + '\'' +
+                ", from=" + from +
+                ", to=" + to +
+                '}';
+    }
+
 }
--- src/main/java/seedu/duke/calendar/EventStorage.java
+++ src/main/java/seedu/duke/calendar/EventStorage.java
@@ -1,4 +1,33 @@
 package seedu.duke.calendar;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 public class EventStorage {
+    ArrayList<Event> events;
+
+    public EventStorage() {
+        events = new ArrayList<>();
+    }
+
+    public void addEvent(String name, LocalDate from, LocalDate to) {
+        events.add(new Event(name, from, to));
+    }
+
+    public boolean delEvent(String name) {
+        for(Event event : events) {
+            if (event.getName().equals(name)) {
+                events.remove(event);
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public String toString() {
+        return "EventStorage{" +
+                "events=" + events +
+                '}';
+    }
 }
