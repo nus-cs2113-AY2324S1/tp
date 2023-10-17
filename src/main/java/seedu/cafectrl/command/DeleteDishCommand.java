02f561afd0972856a346bb73cfd099c3414c1fd7
ShaniceTang <e0959739@u.nus.edu>
2023-10-18 01:17:14 +0800

    Add try-catch error handler

--- src/main/java/seedu/cafectrl/command/DeleteDishCommand.java
+++ src/main/java/seedu/cafectrl/command/DeleteDishCommand.java
@@ -10,6 +10,7 @@ import seedu.cafectrl.ui.Ui;
 public class DeleteDishCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
+    public static final String INVALID_INDEX_MESSAGE = "Please select a valid dish index :)";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the menu item identified by the index number used in the last menu listing.\n"
             + "Parameters: INDEX\n"
@@ -21,12 +22,12 @@ public class DeleteDishCommand extends Command {
 
     @Override
     public void execute(Menu menu, Ui ui) {
-        if (index < 0 || index > menu.getSize()) {
-            ui.showToUser("Please select a valid dish index :)");
-        } else {
+        try {
             Dish selectedDish = menu.getMenuItemsList().get(index - Ui.OFFSET_LIST_INDEX);
             ui.showDeleteMessage(selectedDish);
             menu.removeDish(index);
+        } catch (IndexOutOfBoundsException e) {
+            ui.showToUser(INVALID_INDEX_MESSAGE);
         }
     }
 }
