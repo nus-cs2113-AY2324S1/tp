0f2f80d367518e4a6a47698c6f5eb5911a0f5dd4
ICubE- <jehoyeon01@gmail.com>
2023-10-18 11:19:23 +0800

    Maintain code style

--- src/main/java/fittrack/command/AddWorkoutCommand.java
+++ src/main/java/fittrack/command/AddWorkoutCommand.java
@@ -7,10 +7,11 @@ import fittrack.parser.CommandParser;
 public class AddWorkoutCommand extends Command {
     public static final String COMMAND_WORD = "addworkout";
     private static final String DESCRIPTION =
-            String.format("'%s' adds a workout to the list.", COMMAND_WORD);
+            String.format("`%s` adds a workout to the list.", COMMAND_WORD);
     private static final String USAGE =
-            String.format("Type '%s' <workout> /cals <calories> to add the workout to your list.", COMMAND_WORD);
-    private static final String HELP = DESCRIPTION + "\n" + USAGE;
+            String.format("Type `%s <workout> /cals <calories>` to add the workout to your list.", COMMAND_WORD);
+    public static final String HELP = DESCRIPTION + "\n" + USAGE;
+
     private Workout newWorkout;
 
     @Override
