7f02a0de9aac1ed99944fc0c4beebc648b1d7d60
ShaniceTang <e0959739@u.nus.edu>
2023-10-18 21:26:26 +0800

    Create JUnit test for prepareDelete in Parser class

--- src/test/java/seedu/cafectrl/parser/ParserTest.java
+++ src/test/java/seedu/cafectrl/parser/ParserTest.java
@@ -1,8 +1,25 @@
 package seedu.cafectrl.parser;
 
+import org.junit.jupiter.api.Test;
+import seedu.cafectrl.command.Command;
+import seedu.cafectrl.command.IncorrectCommand;
+import seedu.cafectrl.data.Menu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Junit test for Parser.java
  */
 class ParserTest {
+
+    @Test
+    void parseCommand_missingArg_returnIncorrectCommandObject() {
+        Menu menu = new Menu();
+
+        String fullUserInput = "delete";
+        Command command = Parser.parseCommand(menu, fullUserInput);
+
+        assertEquals(IncorrectCommand.class, command.getClass());
+    }
 
 }
