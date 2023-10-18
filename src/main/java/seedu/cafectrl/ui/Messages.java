d128fc7c9682256fea60137e194f239e52521613
ziyi105 <82555990+ziyi105@users.noreply.github.com>
2023-10-18 11:49:52 +0800

    Messages.java: update position of plus operator for strings

--- src/main/java/seedu/cafectrl/ui/Messages.java
+++ src/main/java/seedu/cafectrl/ui/Messages.java
@@ -5,12 +5,12 @@ public class Messages {
     // Error messages
     public static final String MISSING_ARGUMENT_FOR_EDIT_PRICE = "Error: Missing arguments " +
                                                                     "for edit price command.";
-    public static final String INVALID_DISH_INDEX = "Hmmm, can you double the your dish index, " +
-                                                        "I can't seem to find the dish you're referring to";
-    public static final String WRONG_ARGUMENT_TYPE_FOR_EDIT_PRICE = "Oops, seems like you gave me " +
-                                                                        "the wrong type for dish index " +
-                                                                        "or price. Make sure dish index is " +
-                                                                        "of type int and price is of type float!";
+    public static final String INVALID_DISH_INDEX = "Hmmm, can you double the your dish index, " 
+                                                    + "I can't seem to find the dish you're referring to";
+    public static final String WRONG_ARGUMENT_TYPE_FOR_EDIT_PRICE = "Oops, seems like you gave me "
+                                                                    + "the wrong type for dish index "
+                                                                    + "or price. Make sure dish index is "
+                                                                    + "of type int and price is of type float!";
 
     // Ui messages
     public static final String PRICE_MODIFIED_MESSAGE = "Price modified for the following dish: ";
