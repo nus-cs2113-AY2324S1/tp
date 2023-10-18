41cb964a10164e5c327493bac64d1bb053d05657
Dexter <35828587+DextheChik3n@users.noreply.github.com>
2023-10-18 23:02:34 +0800

    Update ParserTest.java to resolve checkstyle bug

--- src/test/java/seedu/cafectrl/parser/ParserTest.java
+++ src/test/java/seedu/cafectrl/parser/ParserTest.java
@@ -19,7 +19,6 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Junit test for Parser.java
