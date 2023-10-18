25c60717b15629cf088112dfd2068e198239b7c0
ICubE- <jehoyeon01@gmail.com>
2023-10-18 18:51:17 +0800

    Fix test

--- src/main/java/fittrack/command/HelpCommand.java
+++ src/main/java/fittrack/command/HelpCommand.java
@@ -11,7 +11,7 @@ public class HelpCommand extends Command {
     private static final String DESCRIPTION =
             String.format("`%s` shows help message of the command.", COMMAND_WORD);
     private static final String KNOWN_COMMANDS = "Existing commands:\n" + ALL_COMMAND_WORDS;
-    private static final String USAGE =
+    static final String USAGE =
             String.format("Type `%s` or `%s <COMMAND>` to view help.", COMMAND_WORD, COMMAND_WORD);
     public static final String HELP = DESCRIPTION + "\n" + KNOWN_COMMANDS + "\n" + USAGE;
 
