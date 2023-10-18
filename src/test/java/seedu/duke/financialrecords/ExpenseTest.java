97a9b3858d57d9a4bd339e98ed54c363175e83f4
Bryan Lee <e0725090@u.nus.edu>
2023-10-18 15:00:14 +0800

    added balanceTest and javadoc comments for balanceTest and expenseTest

--- src/test/java/seedu/duke/financialrecords/ExpenseTest.java
+++ src/test/java/seedu/duke/financialrecords/ExpenseTest.java
@@ -10,10 +10,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+/**
+ * Test suite for the {@link Expense} class.
+ * This class contains unit tests to validate the behavior of the Expense class and ensure
+ * correctness in the representation and management of financial expenses.
+ */
 public class ExpenseTest {
 
     private Expense expense;
 
+    /**
+     * Initializes common test fixtures before each test execution.
+     */
     @BeforeEach
     void setUp() {
         try {
@@ -23,42 +31,66 @@ public class ExpenseTest {
         }
     }
 
+    /**
+     * Tests if an exception is thrown when the description of an Expense object is empty.
+     */
     @Test
     void testExpenseDescription_notEmpty(){
         assertThrows(KaChinnnngException.class, () -> new Expense("", LocalDate.now(), 20.00));
     }
 
+    /**
+     * Tests if an exception is thrown when the amount of an Expense object is negative.
+     */
     @Test
     void testExpenseAmount_notNegative(){
         assertThrows(KaChinnnngException.class, () -> new Expense("Lunch", LocalDate.now(), -20.00));
     }
 
+    /**
+     * Tests if an exception is thrown when the date of an Expense object is in the future.
+     */
     @Test
     void testExpenseDate_notFuture(){
         assertThrows(KaChinnnngException.class, () -> new Expense("Lunch", LocalDate.now().plusDays(1), 20.00));
     }
 
+    /**
+     * Tests if the correct description of the Expense object is returned.
+     */
     @Test
     void getDescription() {
         assertEquals("Lunch", expense.getDescription());
     }
 
+    /**
+     * Tests if the correct date of the Expense object is returned.
+     */
     @Test
     void getDate() {
         assertEquals(LocalDate.now().minusDays(1), expense.getDate());
     }
 
+    /**
+     * Tests if the correct amount of the Expense object is returned.
+     */
     @Test
     void getAmount() {
         assertEquals(20.00, expense.getAmount());
     }
 
+    /**
+     * Tests if the correct date string of the Expense object is returned.
+     */
     @Test
     void getDateString() {
         assertEquals(LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("dd/MMM/yyyy")),
                 expense.getDateString());
     }
 
+    /**
+     * Tests the string representation of the Expense object.
+     */
     @Test
     void testToString() {
         assertEquals("Expense: Lunch | Date: " +
