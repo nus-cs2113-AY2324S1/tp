a7981fdf23fa49c4f7b677d5b5dfda2fb99225b4
Mark Lin <mark.lin2234@gmail.com>
2023-10-18 10:43:29 +0800

    add delete

--- src/main/java/fittrack/command/DeleteWorkoutCommand.java
+++ src/main/java/fittrack/command/DeleteWorkoutCommand.java
@@ -1,18 +1,24 @@
 package fittrack.command;
 
+import fittrack.WorkoutList;
 import fittrack.parser.CommandParser;
 
 public class DeleteWorkoutCommand extends Command {
     public static final String COMMAND_WORD = "deleteworkout";
 
+    private int index;
+
     @Override
     public CommandResult execute() {
-        return null;
+        WorkoutList.deleteWorkout(index);
+        return new CommandResult("I've deleted workout " + index);
     }
 
     @Override
     public void setArguments(String args, CommandParser parser) {
-
+        String[] input = args.split(" ");
+        index = Integer.parseInt(input[1]);
+        System.out.println(index);
     }
 
     @Override
