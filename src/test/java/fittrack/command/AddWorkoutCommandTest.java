de8722439aab4c629629ef1acbfb3ec2b3d18ff2
Faris Sirraj <faris@curium.sg>
2023-10-18 13:03:04 +0800

    Write test for help - addworkout

--- src/test/java/fittrack/command/AddWorkoutCommandTest.java
+++ src/test/java/fittrack/command/AddWorkoutCommandTest.java
@@ -3,13 +3,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddWorkoutCommandTest {
+    public static final String COMMAND_WORD = "addworkout";
+    private static final String DESCRIPTION =
+            String.format("'%s' adds a workout to the list.", COMMAND_WORD);
+    private static final String USAGE =
+            String.format("Type '%s' <workout> /cals <calories> to add the workout to your list.", COMMAND_WORD);
+    private static final String HELP = DESCRIPTION + "\n" + USAGE;
 
     @Test
     public void testHelp(){
         AddWorkoutCommand addWorkoutCommand = new AddWorkoutCommand();
-        String expected = "'addworkout' adds a workout to the list." + "\n"
-                + "Type 'addworkout' <workout> /cals <calories> to add the workout to your list.";
-        assertEquals(expected, addWorkoutCommand.getHelp());
+        assertEquals(HELP, addWorkoutCommand.getHelp());
     }
 
 
