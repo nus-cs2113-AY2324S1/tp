defbb3e75f20842edf6faa71e54688a3367ed1d6
ziyi105 <82555990+ziyi105@users.noreply.github.com>
2023-10-18 11:58:01 +0800

    Messages.java: Update position of plus operator for MISSING_ARGUMENT_FOR_EDIT_PRICE

--- src/main/java/seedu/cafectrl/ui/Messages.java
+++ src/main/java/seedu/cafectrl/ui/Messages.java
@@ -3,11 +3,11 @@ package seedu.cafectrl.ui;
 public class Messages {
 
     // Error messages
-    public static final String MISSING_ARGUMENT_FOR_EDIT_PRICE = "Error: Missing arguments " +
-                                                                    "for edit price command.";
+    public static final String MISSING_ARGUMENT_FOR_EDIT_PRICE = "Error: Missing arguments "
+                                                                 + "for edit price command.";
     public static final String INVALID_DISH_INDEX = "Hmmm, can you double the your dish index, " 
                                                     + "I can't seem to find the dish you're referring to";
-    public static final String WRONG_ARGUMENT_TYPE_FOR_EDIT_PRICE = "Oops, seems like you gave me "
+    public static final String WRONG_ARGUMENT_TYPE_FOR_EDIT_PRICE = "Oops, seems like you gave me " 
                                                                     + "the wrong type for dish index "
                                                                     + "or price. Make sure dish index is "
                                                                     + "of type int and price is of type float!";
