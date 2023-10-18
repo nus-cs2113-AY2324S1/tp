b18deabd054b19780e3d5cc6eac3fe1e541643d1
Cazh1 <cazh1or2@gmail.com>
2023-10-19 01:35:52 +0800

    Update code to comply Gradle

--- src/main/java/seedu/cafectrl/command/ListMenuCommand.java
+++ src/main/java/seedu/cafectrl/command/ListMenuCommand.java
@@ -22,7 +22,8 @@ public class ListMenuCommand extends Command {
      */
     @Override
     public void execute(Menu menu, Ui ui) {
-        ui.showToUser(UserOutput.MENU_TOP.message, UserOutput.LIST_MESSAGE_MENU.message, UserOutput.MENU_CORNER.message, UserOutput.MENU_TITLE.message, UserOutput.MENU_CORNER.message);
+        ui.showToUser(UserOutput.MENU_TOP.message, UserOutput.LIST_MESSAGE_MENU.message,
+                UserOutput.MENU_CORNER.message, UserOutput.MENU_TITLE.message, UserOutput.MENU_CORNER.message);
         for(int i =0; i < menu.getSize(); i++) {
             String indexNum = String.valueOf(i+1);
             String dishName = menu.getDish(i).getName();
