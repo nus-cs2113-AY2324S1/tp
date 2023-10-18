4d523fe522d1214d635bf46736f2c68a83dd41e5
WooKaiNing <e0968128@u.nus.edu>
2023-10-19 04:14:51 +0800

    Modify runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,20 +1,23 @@
-@echo off
-setlocal enableextensions
-pushd %~dp0
+@ECHO OFF
 
-cd ..
-call gradlew clean shadowJar
+REM create bin directory if it doesn't exist
+if not exist ..\bin mkdir ..\bin
 
-cd build\libs
-for /f "tokens=*" %%a in (
-    'dir /b *.jar'
-) do (
-    set jarloc=%%a
+REM delete output from previous run
+if exist ACTUAL.TXT del ACTUAL.TXT
+
+REM compile the code into the bin folder
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+IF ERRORLEVEL 1 (
+    echo ********** BUILD FAILURE **********
+    exit /b 1
 )
+REM no error here, errorlevel == 0
 
-java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
-java -cp ..\build\libs\cashleh.jar cashleh.CurrentDate
+REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ..\bin CashLeh < input.txt > ACTUAL.TXT
 
-cd ..\..\text-ui-test
+java -cp ..\bin cashleh.CurrentDate
 
-FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
+REM compare the output to the expected output
+FC ACTUAL.TXT EXPECTED.TXT
