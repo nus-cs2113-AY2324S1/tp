ad189e88085884b2a0bd5c8792233896f0f7639d
Ethan <ethanhuang03@gmail.com>
2023-10-19 14:52:14 +0800

    Add tests to Log class

--- src/test/java/seedu/duke/exerciselog/LogTest.java
+++ src/test/java/seedu/duke/exerciselog/LogTest.java
@@ -4,6 +4,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LogTest {
     public static void main(String[] args) {
-        assertEquals("test", "test");
+        LogTest test = new LogTest();
+        test.getNumberOfExercisesTest();
+        test.addAndRemoveExerciseTest();
+    }
+
+    public void getNumberOfExercisesTest() {
+        Log log = new Log();
+        assert log.getNumberOfExercises() == 0;
+        assert log.getNumberOfExercisesForMonth(1) == 0;
+        assert log.getNumberOfExercisesForDay(1, 21) == 0;
+    }
+
+    public void addAndRemoveExerciseTest() {
+        Log log = new Log();
+        log.addExercise(6, 7, "Exercise", 8999999);
+        log.addExercise(8, 24, "Exercise", 112);
+        assert log.getNumberOfExercises() == 2;
+        log.removeExercise(6, 7, "Exercise", 8999999);
+        assert log.getNumberOfExercises() == 1;
+        log.removeExercise(8, 24, "Exercise", 112);
+        assert log.getNumberOfExercises() == 0;
     }
 }
