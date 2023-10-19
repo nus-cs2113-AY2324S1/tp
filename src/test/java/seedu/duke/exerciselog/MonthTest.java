d03e7b422e7b14bda3bb6f4f8e52e86ad358585d
Ethan <ethanhuang03@gmail.com>
2023-10-19 14:17:57 +0800

    Add tests to Month class

--- src/test/java/seedu/duke/exerciselog/MonthTest.java
+++ src/test/java/seedu/duke/exerciselog/MonthTest.java
@@ -4,6 +4,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MonthTest {
     public static void main(String[] args) {
-        assertEquals("test", "test");
+        MonthTest test = new MonthTest();
+        test.createMonthTest();
+        test.addExerciseTest();
+        test.removeExerciseTest();
+    }
+
+    public void createMonthTest() {
+        Month month = new Month(31, "JAN");
+        assert month.getName().equals("JAN");
+        assert month.getNumberOfDays() == 31;
+    }
+
+    public void addExerciseTest() {
+        Month month = new Month(28, "FEB");
+        month.addExercise(3, "Volleyball", 79);
+        assertEquals(month.getNumberOfExercisesForDay(3), 1);
+        month.addExercise(14, "Running", 121);
+        assert month.getNumberOfExercisesForDay(14) == 1;
+        assert month.getTotalNumberOfExercises() == 2;
+    }
+
+    public void removeExerciseTest() {
+        Month month = new Month(28, "FEB");
+        month.addExercise(3, "Volleyball", 79);
+        month.addExercise(14, "Running", 121);
+        month.removeExercise(3, "Volleyball", 79);
+        assert month.getNumberOfExercisesForDay(3) == 0;
+        assert month.getTotalNumberOfExercises() == 1;
     }
 }
