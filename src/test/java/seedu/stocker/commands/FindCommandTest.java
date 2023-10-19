44e4e274eed15e597cb23df11f610716ebd52ccf
barbaracwx <barbara9668@gmail.com>
2023-10-19 10:37:31 +0800

    edited FindCommandTest cases

--- src/test/java/seedu/stocker/commands/FindCommandTest.java
+++ src/test/java/seedu/stocker/commands/FindCommandTest.java
@@ -15,6 +15,8 @@ class FindCommandTest {
         Inventory inventory = new Inventory();
         Drug drug1 = new Drug("Paracetamol", "12/05/2024", 12L);
         inventory.addDrug(drug1);
+        Drug drug2 = new Drug("Panadol", "01/03/2027", 20L);
+        inventory.addDrug(drug2);
 
 
         // Set the modified inventory for the command
@@ -22,11 +24,17 @@ class FindCommandTest {
 
 
         // Define expected output
-        String expectedOutput = "Listed all drugs with the keyword in the inventory.";
+        String expectedOutput = "1. Name: Paracetamol, Expiry Date: 12/05/2024, Quantity: 12" +
+                System.lineSeparator() +
+                "2. Name: Panadol, Expiry Date: 01/03/2027, Quantity: 20" + System.lineSeparator() +
+                System.lineSeparator() +
+                "Listed all drugs with the keyword in the inventory.";
+
+
 
         CommandResult actualResult = command.execute();
 
         // Test the command's execute method with the modified inventory
-        assertEquals(expectedOutput, actualResult.feedbackToUser);
+        assertEquals(expectedOutput, actualResult.getFeedbackToUserFindTest());
     }
 }
