d127d10003da8aae3f4c489a05c8891244b74838
Ethan <ethanhuang03@gmail.com>
2023-10-20 17:53:47 +0800

    Fix checkstyle errors

--- src/test/java/seedu/duke/exerciselog/LogTest.java
+++ src/test/java/seedu/duke/exerciselog/LogTest.java
@@ -1,7 +1,5 @@
 package seedu.duke.exerciselog;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class LogTest {
     public static void main(String[] args) {
         LogTest test = new LogTest();
