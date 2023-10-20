96a2ee87a839774d601f5b5af29e7ba0ae7346a6
danielpappa <daniel.pappalardo@icloud.com>
2023-10-20 17:34:17 +0800

    Add class Budget

---
+++ src/main/java/cashleh/budget/Budget.java
@@ -0,0 +1,29 @@
+package cashleh.budget;
+
+/**
+ * This class creates an object of type Budget which the user can set to manage its
+ * finances. The budget is to be interpreted as the minimum amount of liquid cash
+ * (cash on hand) the user wants to have at all times.
+ * The user can delete the budget by deactivating it.
+ */
+public class Budget {
+    private final double budget;
+    private boolean isActive;
+    public Budget(double budget) {
+        this.budget = budget;
+        this.isActive = true;
+    }
+    public double getBudget() {
+        return Math.abs(this.budget);
+    }
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    public boolean isActive() {
+        return this.isActive;
+    }
+    @Override
+    public String toString() {
+        return "You have set a budget of: " + String.valueOf(this.budget);
+    }
+}
