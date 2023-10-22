a94f8bd0b1e8cfb143320e0ae4c11829a643027a
Faris Sirraj <faris@curium.sg>
2023-10-23 00:59:23 +0800

    Re-write test

--- src/test/java/fittrack/command/AddWorkoutCommandTest.java
+++ src/test/java/fittrack/command/AddWorkoutCommandTest.java
@@ -1,4 +1,7 @@
 package fittrack.command;
+import fittrack.parser.CommandParser;
+import fittrack.Workout;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,9 +13,27 @@ public class AddWorkoutCommandTest {
             String.format("Type '%s' <workout> /cals <calories> to add the workout to your list.", COMMAND_WORD);
     private static final String HELP = DESCRIPTION + "\n" + USAGE;
 
+    private AddWorkoutCommand addWorkoutCommand;
+    private CommandParser parser;
+    final String args = "Workout /cals 100";
+    final double actual = 100;
+
+    @BeforeEach
+    public void setup(){
+        addWorkoutCommand = new AddWorkoutCommand();
+
+    }
+
+    @Test
+    public void setArgumentsTest(){
+        addWorkoutCommand.setArguments(args, parser);
+        double testCals = addWorkoutCommand.getWorkout().getCalories();
+
+        assertEquals(testCals, actual);
+    }
+
     @Test
     public void testHelp(){
-        AddWorkoutCommand addWorkoutCommand = new AddWorkoutCommand();
         assertEquals(HELP, addWorkoutCommand.getHelp());
     }
 
