bf9bd2b8a6eb33b17431bede88dc2a51f58c49c1
Cazh1 <cazh1or2@gmail.com>
2023-10-22 23:54:08 +0800

    Add Order Class

---
+++ src/main/java/seedu/cafectrl/Order.java
@@ -0,0 +1,38 @@
+package seedu.cafectrl;
+
+import seedu.cafectrl.data.dish.Dish;
+import seedu.cafectrl.data.dish.Ingredient;
+
+import java.util.ArrayList;
+
+public class Order {
+    protected String dishName;
+    protected int dishQty;
+    protected Dish orderedDish;
+    protected ArrayList<Ingredient> usedIngredientList;
+    protected boolean isComplete = false;
+    protected float totalOrderCost;
+
+    public Order(String dishName, int dishQty) {
+        this.dishName = dishName;
+        this.dishQty = dishQty;
+    }
+
+    public Order(String dishName, int dishQty, ArrayList<Ingredient> usedIngredientList) {
+        this.dishName = dishName;
+        this.dishQty = dishQty;
+        this.usedIngredientList = usedIngredientList;
+    }
+
+    public Order(String dishName, int dishQty, ArrayList<Ingredient> usedIngredientList, float totalOrderCost) {
+        this.dishName = dishName;
+        this.dishQty = dishQty;
+        this.usedIngredientList = usedIngredientList;
+        this.totalOrderCost = totalOrderCost;
+    }
+
+    public void setComplete() {
+        this.isComplete = true;
+    }
+
+}
