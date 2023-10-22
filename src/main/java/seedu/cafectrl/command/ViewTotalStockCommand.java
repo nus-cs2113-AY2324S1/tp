8a23bd2fcd39cb9e460fd15fa31884e5e6513ad8
ShaniceTang <e0959739@u.nus.edu>
2023-10-22 21:40:01 +0800

    Add view total stock command

--- src/main/java/seedu/cafectrl/command/ViewTotalStockCommand.java
+++ src/main/java/seedu/cafectrl/command/ViewTotalStockCommand.java
@@ -3,19 +3,25 @@ package seedu.cafectrl.command;
 import seedu.cafectrl.data.Menu;
 import seedu.cafectrl.data.Pantry;
 import seedu.cafectrl.data.dish.Ingredient;
+import seedu.cafectrl.ui.Messages;
 import seedu.cafectrl.ui.Ui;
 
 import java.util.ArrayList;
 
 public class ViewTotalStockCommand extends Command {
 
-    private ArrayList<Ingredient> pantryStock;
     public static final String COMMAND_WORD = "view_stock";
-
+    private ArrayList<Ingredient> pantryStock;
     private Pantry pantry;
+
+    public ViewTotalStockCommand() {
+        this.pantry = new Pantry();
+    }
 
     @Override
     public void execute(Menu menu, Ui ui) {
+
+        ui.showToUser(Messages.VIEW_STOCK);
         pantryStock = pantry.getPantryStock();
 
         for (Ingredient ingredient : pantryStock) {
