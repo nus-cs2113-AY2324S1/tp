a35e40f8136d9c95a5ec2e100a5e930cfc86c5a5
Cazh1 <cazh1or2@gmail.com>
2023-10-24 02:28:31 +0800

    Update AddOrderCommand to use Chef

--- src/main/java/seedu/cafectrl/command/AddOrderCommand.java
+++ src/main/java/seedu/cafectrl/command/AddOrderCommand.java
@@ -1,28 +1,25 @@
 package seedu.cafectrl.command;
 
+import seedu.cafectrl.Chef;
 import seedu.cafectrl.Order;
 import seedu.cafectrl.data.Menu;
+import seedu.cafectrl.data.Pantry;
 import seedu.cafectrl.ui.Ui;
 
 public class AddOrderCommand extends Command {
     public static final String COMMAND_WORD = "add_order";
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Add dish to menu\n"
-            + "Parameters: DISH_NAME, PRICE, INGREDIENT1_NAME, INGREDIENT1_QTY "
-            + "[,INGREDIENT1_NAME, INGREDIENT1_QTY...] \n"
+            + ": Add order to orders\n"
+            + "Parameters: DISH_NAME, DISH_QTY\n"
             + "Example: " + COMMAND_WORD
-            + " name/chicken rice price/3.00 ingredient/rice qty/1 cup, ingredient/chicken qty/100g";
+            + " name/chicken rice qty/2";
     Order order;
     public AddOrderCommand(Order order) {
         this.order  = order;
     }
-    /*@Override
-    public void execute(OrderList orderList, Ui ui) {
-        orderList.addOrder(order);
-        ui.printAddDishMessage(dish);
-    }*/
     @Override
-    public void execute(Menu menu, Ui ui) {
-
+    public void execute(Menu menu, Ui ui, Pantry pantry) {
+        Chef chef = new Chef(order, pantry);
+        chef.cookDish();
     }
 }
