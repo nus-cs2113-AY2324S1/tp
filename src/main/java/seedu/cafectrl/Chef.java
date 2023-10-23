9977e9dbef1cb6980d68e58c14a5fd40a843a711
Cazh1 <cazh1or2@gmail.com>
2023-10-24 03:53:48 +0800

    Update Chef.java

--- src/main/java/seedu/cafectrl/Chef.java
+++ src/main/java/seedu/cafectrl/Chef.java
@@ -1,22 +1,30 @@
 package seedu.cafectrl;
 
 import seedu.cafectrl.data.Pantry;
+import seedu.cafectrl.ui.Ui;
 
 public class Chef {
     protected Order order;
     protected Pantry pantry;
-    public Chef(Order order, Pantry pantry) {
+    protected Ui ui;
+    public Chef(Order order, Pantry pantry, Ui ui) {
         this.order = order;
         this.pantry = pantry;
+        this.ui = ui;
     }
 
     public void cookDish() {
         try {
             pantry.decreaseIngredientsStock(order.usedIngredientList);
             order.setComplete();
-            System.out.println("Is order completed?: " + order.isComplete);
         } catch (Exception e) {
-            System.out.println("Unable to cook: " + e.getMessage());
+            ui.showToUser(e.getMessage());
+        } finally {
+            if (order.isComplete) {
+                ui.showToUser("Order was completed");
+            } else {
+                ui.showToUser("Order not completed");
+            }
         }
     }
 }
