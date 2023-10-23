dc34985320982b29e14daf85c679fe7db0328386
Xuan127 <shanyuxuan2001@gmail.com>
2023-10-23 20:22:01 +0800

    Add categories

--- src/main/java/cashleh/transaction/Transaction.java
+++ src/main/java/cashleh/transaction/Transaction.java
@@ -1,17 +1,17 @@
 package cashleh.transaction;
 
 import java.time.LocalDate;
+
 /**
  * Abstract class providing common structure for financial transactions,
  * including a description, an amount, and an optional date.
  */
-
 public abstract class Transaction {
 
     private String description;
     private double amount;
-
-    private LocalDate date;
+    private LocalDate date = null;
+    private Categories category = null;
 
     /*
      * Constructor for Transaction - set date to current date if not specified
@@ -36,6 +36,32 @@ public abstract class Transaction {
         this.date = date;
     }
 
+    /*
+     * Constructor for Transaction
+     * @param description Description of the transaction
+     * @param amount Amount of the transaction
+     * @param category Category of the transaction
+     */
+    public Transaction(String description, double amount, Categories category) {
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    /*
+     * Constructor for Transaction
+     * @param description Description of the transaction
+     * @param amount Amount of the transaction
+     * @param date Date of the transaction
+     * @param category Category of the transaction
+     */
+    public Transaction(String description, double amount, LocalDate date, Categories category) {
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+        this.category = category;
+    }
+
     public double getAmount() {
         return amount;
     }
@@ -46,6 +72,10 @@ public abstract class Transaction {
 
     public LocalDate getDate() {
         return date;
+    }
+
+    public Categories getCategory() {
+        return category;
     }
 
     public void setAmount(double amount) {
@@ -60,5 +90,9 @@ public abstract class Transaction {
         this.date = date;
     }
 
+    public void setCategory(Categories category) {
+        this.category = category;
+    }
+    
     public abstract String toString();
 }
