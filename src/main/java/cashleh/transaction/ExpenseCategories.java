302c56df68b13ed3a2b97fec54e8bbe86d2e301d
Xuan127 <shanyuxuan2001@gmail.com>
2023-10-23 20:22:47 +0800

    Add categories and modify Income and Expense class

---
+++ src/main/java/cashleh/transaction/ExpenseCategories.java
@@ -0,0 +1,13 @@
+package cashleh.transaction;
+
+public class ExpenseCategories implements Categories {
+    public enum IncomeCategory {
+        FOOD_DRINK,
+        SHOPPING,
+        HOUSING,
+        TRANSPORTATION,
+        ENTERTAINMENT,
+        UTILITIES,
+        OTHERS
+    }
+}
---
+++ src/main/java/cashleh/transaction/IncomeCategories.java
@@ -0,0 +1,11 @@
+package cashleh.transaction;
+
+public class IncomeCategories implements Categories {
+    public enum IncomeCategory {
+        SALARY,
+        ALLOWANCE,
+        INVESTMENT,
+        LOTTERY_GAMBLING,
+        OTHERS
+    }
+}
