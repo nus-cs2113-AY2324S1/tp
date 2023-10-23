bfa0891e46c52dcfb3c301d159915bc0d5fcfaf7
Cazh1 <cazh1or2@gmail.com>
2023-10-24 02:28:06 +0800

    Create Chef class

---
+++ src/main/java/seedu/cafectrl/Chef.java
@@ -0,0 +1,23 @@
+package seedu.cafectrl;
+
+import seedu.cafectrl.data.Pantry;
+
+public class Chef {
+    protected Order order;
+    protected Pantry pantry;
+    public Chef(Order order, Pantry pantry) {
+        this.order = order;
+        this.pantry = pantry;
+    }
+
+    public void cookDish() {
+        try {
+            pantry.decreaseIngredientsStock(order.usedIngredientList);
+            order.setComplete();
+            System.out.println("Is order completed?: " + order.isComplete);
+            pantry.printPantryStock();
+        } catch (Exception e) {
+            System.out.println("Unable to cook: " + e.getMessage());
+        }
+    }
+}
