0aa3c28f9bf7bd41d534d1aae5e3e7f6bc391b31
James-Hong-Jey <jameshong8387@gmail.com>
2023-10-25 15:24:22 +0800

    Fix 1 Junit, 2 left to fix

--- src/test/java/quizhub/command/CommandHelpTest.java
+++ src/test/java/quizhub/command/CommandHelpTest.java
@@ -52,6 +52,7 @@ public class CommandHelpTest {
      * */
     @Test
     public void testHelpCommand() {
+        /*
         String expectedOutput = "Here are the list of commands you can use:\r\n" +
                 "    1. help - shows the list of commands you can use\r\n" +
                 "    2. short [question]/[answer] - adds a short answer question and its answer to the list,\r\n" +
@@ -68,6 +69,39 @@ public class CommandHelpTest {
                 "     10. markdiff [question number] [question difficulty] - sets the difficulty of question " +
                 "with the specified number,\r\n" +
                 "     11. bye - exits the program";
+         */
+
+        String expectedOutput= "Here are the list of commands you can use:" +
+                System.lineSeparator() +
+                "    1. help - shows the list of commands you can use" +
+                System.lineSeparator() +
+                "    2. short [question]/[answer]/[module]/[difficulty] - adds a short answer question and " +
+                "its answer to the list," +
+                System.lineSeparator() +
+                "     3. list - shows the list of questions and answers," +
+                System.lineSeparator() +
+                "     4. delete [question number] - deletes the question and answer at the specified number," +
+                System.lineSeparator() +
+                "     5. find /[description] - displays all questions that contains the the specified description," +
+                System.lineSeparator() +
+                "     6. find /[module] - displays all questions that belong to the specified module," +
+                System.lineSeparator() +
+                "     7. edit [question number] /description [description] - edits the description of the question " +
+                "with the specified number," +
+                System.lineSeparator() +
+                "     8. edit [question number] /answer [answer] - edits the answer to the question with " +
+                "the specified number," +
+                System.lineSeparator() +
+                "     9. start /[quiz mode] [start details] /[qn mode] - " +
+                "starts the quiz with option for /module or /all and /random or /normal," +
+                System.lineSeparator() +
+                "     10. shuffle - shuffle quiz questions to a random order," +
+                System.lineSeparator() +
+                "     11. markdiff [question number] [question difficulty] - sets the difficulty of question " +
+                "with the specified number," +
+                System.lineSeparator() +
+                "     12. bye - exits the program";
+
         CommandHelp help = new CommandHelp();
         help.executeCommand(mockUi, mockStorage, questionList);
         testCliOutputCorrectness(expectedOutput);
