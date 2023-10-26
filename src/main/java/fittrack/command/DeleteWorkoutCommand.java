55de4d05b7dff72b622e2e76e7c22a8ac9d8cca8
Faris Sirraj <faris@curium.sg>
2023-10-26 12:52:27 +0800

    Import storage class

--- src/main/java/fittrack/command/DeleteWorkoutCommand.java
+++ src/main/java/fittrack/command/DeleteWorkoutCommand.java
@@ -5,6 +5,7 @@ import fittrack.parser.CommandParser;
 import fittrack.parser.IndexOutOfBoundsException;
 import fittrack.parser.NumberFormatException;
 import fittrack.parser.PatternMatchFailException;
+import fittrack.storage.Storage;
 
 public class DeleteWorkoutCommand extends Command {
     public static final String COMMAND_WORD = "deleteworkout";
