7a5f4c68f5ac474879c049c00b18917835693a42
Faris Sirraj <faris@curium.sg>
2023-10-26 12:52:07 +0800

    Update method to throw exception

--- src/main/java/fittrack/command/DeleteMealCommand.java
+++ src/main/java/fittrack/command/DeleteMealCommand.java
@@ -23,7 +23,7 @@ public class DeleteMealCommand extends Command {
 
     // @@author NgLixuanNixon
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() throws Storage.StorageOperationException {
         if (!mealList.isIndexValid(mealIndex)) {
             return new CommandParser()
                     .getInvalidCommand(commandLine, new IndexOutOfBoundsException())
