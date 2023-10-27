32025f87af667fe81815c327984f6b4ce357822c
ZH <cazh1or2@gmail.com>
2023-10-27 14:25:35 +0800

    Update Pantry.java for AddOrderCommand JUnit

--- src/main/java/seedu/cafectrl/data/Pantry.java
+++ src/main/java/seedu/cafectrl/data/Pantry.java
@@ -122,7 +122,7 @@ public class Pantry {
      * @param dishIngredients Array of ingredients used to make the dish order.
      */
     public void decreaseIngredientsStock(ArrayList<Ingredient> dishIngredients){
-        pantryStock = retrieveStockFromStorage();
+        //pantryStock = retrieveStockFromStorage();
 
         //for each ingredient that is used in the dish, update the stock of ingredient left.
         for (Ingredient dishIngredient : dishIngredients) {
