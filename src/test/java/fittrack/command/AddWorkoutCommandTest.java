4363af87cb11a66339b4325f267858d9272be373
ICubE- <jehoyeon01@gmail.com>
2023-10-28 15:50:41 +0800

    Update AddWorkoutCommandTest.java

--- src/test/java/fittrack/command/AddWorkoutCommandTest.java
+++ src/test/java/fittrack/command/AddWorkoutCommandTest.java
@@ -1,38 +1,29 @@
 package fittrack.command;
 import fittrack.parser.CommandParser;
-import org.junit.jupiter.api.BeforeEach;
+import fittrack.parser.ParseException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// @@author farissirraj
 public class AddWorkoutCommandTest {
-    public static final String COMMAND_WORD = "addworkout";
-    private static final String DESCRIPTION =
-            String.format("'%s' adds a workout to the list.", COMMAND_WORD);
-    private static final String USAGE =
-            String.format("Type '%s' <workout> /cals <calories> to add the workout to your list.", COMMAND_WORD);
-    private static final String HELP = DESCRIPTION + "\n" + USAGE;
-
-    private AddWorkoutCommand addWorkoutCommand;
-
-    @BeforeEach
-    public void setup(){
-        addWorkoutCommand = new AddWorkoutCommand();
-    }
 
     @Test
     public void setArgumentsTest(){
-        String args = "Workout /cals 100";
+        String args = "Workout c/100";
+        AddWorkoutCommand addWorkoutCommand = new AddWorkoutCommand("addworkout " + args);
         double actual = 100;
         CommandParser parser = new CommandParser();
-        addWorkoutCommand.setArguments(args, parser);
-        double testCals = addWorkoutCommand.getWorkout().getCalories();
-        assertEquals(testCals, actual);
+        try {
+            addWorkoutCommand.setArguments(args, parser);
+            double testCals = addWorkoutCommand.getWorkout().getCalories();
+            assertEquals(testCals, actual);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     public void testHelp(){
-        assertEquals(HELP, addWorkoutCommand.getHelp());
+        assertEquals(AddWorkoutCommand.HELP, new AddWorkoutCommand("").getHelp());
     }
-
-
 }
