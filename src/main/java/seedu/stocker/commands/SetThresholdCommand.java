12cc11258a04b1d8e52c7c7ddcf74a9d0df1043d
barbaracwx <barbara9668@gmail.com>
2023-10-30 14:14:03 +0800

    added javadoc comments to set threshold command

--- src/main/java/seedu/stocker/commands/SetThresholdCommand.java
+++ src/main/java/seedu/stocker/commands/SetThresholdCommand.java
@@ -2,7 +2,9 @@ package seedu.stocker.commands;
 
 import seedu.stocker.drugs.StockEntry;
 
-
+/**
+ * Represents a command to set the threshold quantity for a drug in the inventory.
+ */
 public class SetThresholdCommand extends Command {
 
     public static final String COMMAND_WORD = "setthreshold";
@@ -17,18 +19,31 @@ public class SetThresholdCommand extends Command {
     private final String drugName;
     private final long threshold;
 
+    /**
+     * Constructs a SetThresholdCommand with a specified drug name and threshold quantity.
+     * @param name The name of the drug.
+     * @param threshold The threshold quantity to set.
+     */
     public SetThresholdCommand(String name, long threshold) {
         this.drugName = name;
         this.threshold = threshold;
     }
 
+    /**
+     * Constructs a SetThresholdCommand with a specified drug name and a default threshold quantity of 100.
+     * @param name The name of the drug.
+     */
     public SetThresholdCommand(String name) {
         this.drugName = name;
         this.threshold = 100; // Default threshold value
     }
 
+    /**
+     * Executes the SetThresholdCommand by setting the threshold quantity for the specified drug.
+     * @return A CommandResult indicating the success or failure of the command.
+     */
     @Override
-    public CommandResult execute(){
+    public CommandResult execute() {
         StockEntry stockEntry = inventory.getStockEntry(drugName);
 
         if (stockEntry != null) {
@@ -39,4 +54,3 @@ public class SetThresholdCommand extends Command {
         }
     }
 }
-
