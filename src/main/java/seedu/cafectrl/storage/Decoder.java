eb4fe9c2be104ced8c51c20b6fabed88a32034c1
ShaniceTang <e0959739@u.nus.edu>
2023-10-30 20:44:46 +0800

    Implement decoder for Menu to parse text file as ArrayList

--- src/main/java/seedu/cafectrl/storage/Decoder.java
+++ src/main/java/seedu/cafectrl/storage/Decoder.java
@@ -6,10 +6,52 @@ import seedu.cafectrl.data.dish.Dish;
 import seedu.cafectrl.data.dish.Ingredient;
 import seedu.cafectrl.ui.Ui;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
+
+//from txt file to arraylist
 public class Decoder {
     private static final Ui ui = new Ui();
+
+    /**
+     * Decodes an ArrayList of string lines into a Menu object, reconstructing its content.
+     *
+     * @param textLines An ArrayList of strings representing the encoded Menu data.
+     * @return A Menu object containing the decoded Menu data.
+     */
+    public static Menu decodeMenuData(ArrayList<String> textLines) {
+        ArrayList<Dish> menuDishList = new ArrayList<>();
+        for(String dishString : textLines){
+            String[] dishStringArray = dishString.split(" \\| ");
+            String dishName = dishStringArray[0];
+            float dishPrice = Float.parseFloat(dishStringArray[1]);
+            String[] ingredientStringArray = Arrays.copyOfRange(dishStringArray, 2, dishStringArray.length);
+            ArrayList<Ingredient> ingredientsList = decodeIngredientData(ingredientStringArray);
+            menuDishList.add(new Dish(dishName, ingredientsList, dishPrice));
+        }
+        return new Menu(menuDishList);
+    }
+
+    /**
+     * Decodes an array of strings representing ingredient data into a list of Ingredient objects.
+     *
+     * @param ingredientsStringArray An array of strings containing encoded ingredient data.
+     * @return An ArrayList of Ingredient objects containing the decoded ingredient information.
+     */
+    private static ArrayList<Ingredient> decodeIngredientData(String[] ingredientsStringArray) {
+        ArrayList<Ingredient> ingredientList = new ArrayList<>();
+        for(String ingredientString : ingredientsStringArray) {
+            String[] array = ingredientString.split(" ");
+            String name = array[0];
+            int qty = Integer.parseInt(array[1]);
+            String unit = array[2];
+            ingredientList.add(new Ingredient(name, qty, unit));
+        }
+        return ingredientList;
+    }
+
     public static Pantry decodePantryStockData(ArrayList<String> encodedPantryStock) {
         ArrayList<Ingredient> pantryStock = new ArrayList<>();
         for (String encodedData : encodedPantryStock) {
@@ -35,24 +77,4 @@ public class Decoder {
         return true;
     }
 
-    //@@author Dexter
-    public Menu decodeMenuData(ArrayList<String> textLines) {
-        ArrayList<Dish> dishArrayList = new ArrayList<>();
-        for (String task : textLines) {
-            String[] splitTaskString = task.split(" \\| ");
-            String dishName = splitTaskString[0];
-            float dishPrice = Float.parseFloat(splitTaskString[1]);
-            String dishIngredient = splitTaskString[2];
-
-            try {
-                //todo: remove testing code
-                Dish dish = new Dish(dishName, dishPrice);
-                dishArrayList.add(dish);
-            } catch (Exception e) {
-                ui.showToUser(e.getMessage());
-            }
-        }
-
-        return new Menu(dishArrayList);
-    }
 }
