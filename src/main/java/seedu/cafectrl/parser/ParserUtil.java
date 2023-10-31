f23ad963a59d4582af953707581c8421250f13bd
ziyi105 <ahziyi@gmail.com>
2023-10-31 10:50:21 +0800

    Create ParserUtil.java interface

---
+++ src/main/java/seedu/cafectrl/parser/ParserUtil.java
@@ -0,0 +1,16 @@
+package seedu.cafectrl.parser;
+
+import seedu.cafectrl.command.Command;
+import seedu.cafectrl.data.CurrentDate;
+import seedu.cafectrl.data.Menu;
+import seedu.cafectrl.data.Pantry;
+import seedu.cafectrl.data.Sales;
+import seedu.cafectrl.ui.Ui;
+
+/**
+ * Parser interface for external class to use Parser
+ */
+public interface ParserUtil {
+    Command parseCommand(Menu menu, String userInput, Ui ui,
+                                Pantry pantry, Sales sales, CurrentDate currentDate);
+}
