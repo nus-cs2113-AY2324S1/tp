f4d5da8a6e8651b6d5feb7cf4d82fd859cf10990
J0shuaLeong <joshualeong2607@gmail.com>
2023-10-31 13:09:16 +0800

    Removed unused exceptions

--- src/test/java/fittrack/storage/StorageTest.java
+++ src/test/java/fittrack/storage/StorageTest.java
@@ -13,13 +13,13 @@ public class StorageTest {
     private static final String TEST_DATA_FOLDER = "test/data/StorageFileTest";
 
     @Test
-    public void constructor_nullFilePath_exceptionThrown() throws Exception {
+    public void constructor_nullFilePath_exceptionThrown() {
         assertThrows(NullPointerException.class,
                 () -> new Storage(null, null, null));
     }
 
     @Test
-    public void load_invalidFormat_exceptionThrown() throws Exception {
+    public void load_invalidProfileFormat_exceptionThrown() throws Exception {
         // The file contains valid txt data, but does not match the format
         Storage storage = getStorage("InvalidProfileData.txt",
                 "InvalidMealListData.txt", "InvalidWorkoutListData.txt");
@@ -32,12 +32,6 @@ public class StorageTest {
         assertThrows(NullPointerException.class, () -> storage.saveProfile(null));
         assertThrows(NullPointerException.class, () -> storage.saveMeals(null));
         assertThrows(NullPointerException.class, () -> storage.saveWorkouts(null));
-    }
-
-    private Storage getTempStorage() throws Exception {
-        return new Storage(testFolder.resolve("temp.txt").toString(),
-                testFolder.resolve("temp.txt").toString(),
-                testFolder.resolve("temp.txt").toString());
     }
 
     private Storage getStorage(String profileFileName, String mealFileName, String workoutFileName)
