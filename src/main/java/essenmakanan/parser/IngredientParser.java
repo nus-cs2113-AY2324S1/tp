917ffba54a8beafeb0b4fc3d623cbeee9a90b8d0
Stanley Wijaya <wijayastanley00@gmail.com>
2023-11-01 00:42:49 +0800

    Fix style issue in ingredient parser

--- src/main/java/essenmakanan/parser/IngredientParser.java
+++ src/main/java/essenmakanan/parser/IngredientParser.java
@@ -5,10 +5,6 @@ import essenmakanan.exception.EssenOutOfRangeException;
 import essenmakanan.ingredient.Ingredient;
 import essenmakanan.ingredient.IngredientList;
 import essenmakanan.ingredient.IngredientUnit;
-import essenmakanan.recipe.Step;
-
-import java.util.ArrayList;
-import java.util.StringJoiner;
 
 public class IngredientParser {
     public static int getIngredientIndex(IngredientList ingredients, String input)
