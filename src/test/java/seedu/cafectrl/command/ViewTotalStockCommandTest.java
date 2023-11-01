ec613ebbb2c1bae995517c2afcb1ed10f53fce3f
ShaniceTang <e0959739@u.nus.edu>
2023-11-01 13:27:28 +0800

    Implement JUnit test for ViewTotalStock execute command
    Fixes #188

---
+++ src/test/java/seedu/cafectrl/command/ViewTotalStockCommandTest.java
@@ -0,0 +1,43 @@
+package seedu.cafectrl.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.cafectrl.data.Pantry;
+import seedu.cafectrl.data.dish.Ingredient;
+import seedu.cafectrl.ui.Ui;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ViewTotalStockCommandTest {
+    @Test
+    void execute_printPantryStock() {
+        ArrayList<Ingredient> pantryStock = new ArrayList<>();
+        pantryStock.add(new Ingredient("chicken", 500, "g"));
+        pantryStock.add(new Ingredient("rice", 1000, "g"));
+
+        Ui ui = new Ui();
+        Pantry pantry = new Pantry(ui, pantryStock);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream consoleStream = new PrintStream(baos);
+
+        PrintStream originalOut = System.out;
+        System.setOut(consoleStream);
+
+        ViewTotalStockCommand viewTotalStockCommand = new ViewTotalStockCommand(pantry, ui);
+        viewTotalStockCommand.execute();
+
+        String actualOutput = baos.toString().trim();
+        System.setOut(originalOut);
+
+        String expectedOutput = "You have the following ingredients in pantry:\n"
+                + "Ingredients\t\tQty\n"
+                + "chicken\t\t\t500g\n"
+                + "rice\t\t\t1000g";
+
+        assertEquals(expectedOutput.trim().replaceAll("\\s+", " "),
+                actualOutput.trim().replaceAll("\\s+", " "));
+    }
+}
