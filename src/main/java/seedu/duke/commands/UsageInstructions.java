cc4cffd2d86c646768194f84f0c1b2e72586f3db
KenCheung18 <kycheungbf@connect.ust.hk>
2023-11-01 21:20:18 +0800

    help function update for currency update

--- src/main/java/seedu/duke/commands/UsageInstructions.java
+++ src/main/java/seedu/duke/commands/UsageInstructions.java
@@ -37,7 +37,9 @@ public class UsageInstructions {
                 "edit expense <index> /cat <catergory> /type <type> /de <description> /date <date> " +
                 "/amt [currency] <amount>\n");
         System.out.println("Check balance of income: balance\nFormat: " + "balance\n");
+        System.out.println("Update exchange rate: update exchange rate\nFormat: " + "update exchange rate <exchange currency><exchange rate>\n");
         System.out.println("Exiting the program: exit\nFormat: " + "exit");
+
         ui.showLineDivider();
     }
 }
