7ab78b8caa3152372260a6fef318061c9e69f9da
NaychiMin <naychimin02@gmail.com>
2023-11-01 10:42:58 +0800

    Create ShowSalesByDayCommand class.
    Fixes #189

--- src/main/java/seedu/cafectrl/command/ShowSalesByDayCommand.java
+++ src/main/java/seedu/cafectrl/command/ShowSalesByDayCommand.java
@@ -1,2 +1,26 @@
-package seedu.cafectrl.command;public class ShowSalesByDayCommand {
+package seedu.cafectrl.command;
+
+import seedu.cafectrl.data.Menu;
+import seedu.cafectrl.data.Sales;
+import seedu.cafectrl.ui.Ui;
+
+public class ShowSalesByDayCommand extends Command{
+    public static final String COMMAND_WORD = "show_sale";
+
+    private final int day;
+    private final Ui ui;
+    private final Sales sales;
+    private final Menu menu;
+
+    public ShowSalesByDayCommand(int day, Ui ui, Sales sales, Menu menu) {
+        this.day = day;
+        this.ui = ui;
+        this.sales = sales;
+        this.menu = menu;
+    }
+
+    @Override
+    public void execute() {
+        sales.printSaleByDay(ui, menu, day);
+    }
 }
