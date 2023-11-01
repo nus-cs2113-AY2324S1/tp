9dece87746f697fb91b7dfdfc5d0f49b8e2e6e05
NaychiMin <naychimin02@gmail.com>
2023-11-01 10:43:45 +0800

    Create ShowSalesCommand class.
    Fixes #189

--- src/main/java/seedu/cafectrl/command/ShowSalesCommand.java
+++ src/main/java/seedu/cafectrl/command/ShowSalesCommand.java
@@ -1,2 +1,32 @@
-package seedu.cafectrl.command;public class ShowSalesCommand {
+package seedu.cafectrl.command;
+
+import seedu.cafectrl.data.Menu;
+import seedu.cafectrl.data.Sales;
+import seedu.cafectrl.data.dish.Dish;
+import seedu.cafectrl.ui.ErrorMessages;
+import seedu.cafectrl.ui.Ui;
+
+public class ShowSalesCommand extends Command{
+    public static final String COMMAND_WORD = "show_sales";
+    private Sales sales;
+    private Menu menu;
+
+    //change later
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Lists out the ingredients needed along with the quantity for a specific dish.\n"
+            + "Parameters: INDEX\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    protected Ui ui;
+
+    public ShowSalesCommand(Sales sales, Ui ui, Menu menu) {
+        this.ui = ui;
+        this.menu = menu;
+        this.sales = sales;
+    }
+
+    @Override
+    public void execute() {
+        sales.printSales(ui, menu);
+    }
 }
