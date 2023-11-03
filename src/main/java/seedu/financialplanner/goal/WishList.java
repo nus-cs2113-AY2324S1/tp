47187ceb82e2de382e24fb010cb25952cb6f1967
Shi Haochen <99376359+hshiah@users.noreply.github.com>
2023-11-03 10:30:12 +0800

    Update WishList.java

--- src/main/java/seedu/financialplanner/goal/WishList.java
+++ src/main/java/seedu/financialplanner/goal/WishList.java
@@ -30,7 +30,6 @@ public class WishList {
         for (int i = 0; i < list.size(); i++) {
             if (i == list.size() - 1) {
                 result += String.format("%d. %s", i + 1, list.get(i));
-                break;
             } else {
                 result += String.format("%d. %s\n", i + 1, list.get(i));
             }
