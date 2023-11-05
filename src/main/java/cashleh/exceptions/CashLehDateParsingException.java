0fb2e25ac6cdf1c87ab5679193bac0b2e6d3ac71
mcmc101001 <tohmingchun101001@gmail.com>
2023-11-05 21:21:42 +0800

    Modify date exception message

--- src/main/java/cashleh/exceptions/CashLehDateParsingException.java
+++ src/main/java/cashleh/exceptions/CashLehDateParsingException.java
@@ -7,6 +7,6 @@ public class CashLehDateParsingException extends CashLehParsingException {
      *
      */
     public CashLehDateParsingException() {
-        super("Date format is invalid leh! Use a real date in DD/MM/YYYY can or not?");
+        super("Date format is invalid leh! Use a valid date in DD/MM/YYYY can or not?");
     }
 }
