47cd9c4cbec1f91749ad7d4efa17943986024b60
Faris Sirraj <faris@curium.sg>
2023-11-07 02:12:05 +0800

    Create new class for command to add steps to the list

---
+++ src/main/java/fittrack/command/AddStepsCommand.java
@@ -0,0 +1,59 @@
+package fittrack.command;
+
+import fittrack.data.Step;
+import fittrack.parser.CommandParser;
+import fittrack.parser.ParseException;
+
+public class AddStepsCommand extends Command{
+    public static final String COMMAND_WORD = "addsteps";
+    private static final String DESCRIPTION =
+            String.format("`%s` shows your total number of steps on a specific date.", COMMAND_WORD);
+    private static final String USAGE = String.format(
+            "Type `%s <DATE>` to see the total steps walked on that date.\n" +
+                    "You should type <DATE> in format of `yyyy-MM-dd`.",
+            COMMAND_WORD
+    );
+    public static final String HELP = DESCRIPTION + "\n" + USAGE;
+
+    private Step newStep;
+
+    public AddStepsCommand(String commandLine) {
+        super(commandLine);
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return result of the execution
+     */
+    @Override
+    public CommandResult execute() {
+        stepList.addToList(newStep);
+        return new CommandResult("I've added the following steps:" + "\n" + newStep.toString());
+    }
+
+    /**
+     * Apply arguments to its field using parser.
+     *
+     * @param args   arguments as a string
+     * @param parser parser
+     * @throws ParseException if parse fails
+     */
+    @Override
+    public void setArguments(String args, CommandParser parser) throws ParseException {
+        newStep = parser.parseStep(args);
+    }
+
+    public Step getStep(){
+        return newStep;
+    }
+    /**
+     * Returns help of the command.
+     *
+     * @return help
+     */
+    @Override
+    protected String getHelp() {
+        return null;
+    }
+}
