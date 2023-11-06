50fdcdafafac8fa0a1976f5681123ffa393e27c8
Faris Sirraj <faris@curium.sg>
2023-11-07 02:12:40 +0800

    Create new class for command to calculate the total number of steps travelled

---
+++ src/main/java/fittrack/command/TotalStepsCommand.java
@@ -0,0 +1,44 @@
+package fittrack.command;
+
+import fittrack.data.Step;
+import fittrack.parser.CommandParser;
+import fittrack.parser.ParseException;
+import fittrack.parser.PatternMatchFailException;
+
+public class TotalStepsCommand extends Command{
+    public static final String COMMAND_WORD = "totalsteps";
+    private static final String DESCRIPTION = "`" + COMMAND_WORD + "` shows the total number of steps taken.";
+    private static final String USAGE = "Type `totalsteps` to show the total number of steps taken.";
+    public static final String HELP = DESCRIPTION + "\n" + USAGE;
+
+    private int totalSteps = 0;
+
+    public TotalStepsCommand(String commandLine) {
+        super(commandLine);
+    }
+
+    @Override
+    public CommandResult execute() {
+        for (Step step: stepList.getStepList()) {
+            totalSteps += step.getSteps();
+        }
+        return new CommandResult("Total steps taken: " + totalSteps + " steps");
+    }
+
+    /**
+     * Apply arguments to its field using parser.
+     *
+     * @param args   arguments as a string
+     * @param parser parser
+     * @throws ParseException if parse fails
+     */
+    @Override
+    public void setArguments(String args, CommandParser parser) throws ParseException {
+    }
+
+
+    @Override
+    protected String getHelp() {
+        return HELP;
+    }
+}
