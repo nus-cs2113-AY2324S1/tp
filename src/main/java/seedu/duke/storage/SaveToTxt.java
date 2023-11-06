7d40e19f76aed49ba358c678c82881a0d5e60623
KenCheung18 <kycheungbf@connect.ust.hk>
2023-11-06 22:54:32 +0800

    change the storage to deal with | and log

--- src/main/java/seedu/duke/storage/SaveToTxt.java
+++ src/main/java/seedu/duke/storage/SaveToTxt.java
@@ -1,13 +1,23 @@
 package seedu.duke.storage;
 
+import seedu.duke.commands.KaChinnnngException;
 import seedu.duke.financialrecords.Income;
 import seedu.duke.financialrecords.Expense;
 import seedu.duke.financialrecords.Food;
 import seedu.duke.financialrecords.Utilities;
 import seedu.duke.financialrecords.Transport;
+import seedu.duke.parser.Parser;
+
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * The SaveToTxt class is responsible for saving financial records data to a text file.
@@ -16,8 +26,37 @@ import java.util.ArrayList;
  * This class handles exceptions for IO errors that may occur during file operations.
  */
 public class SaveToTxt {
+    private static final DecimalFormat df = new DecimalFormat("0.00");
     private static final String IO_ERROR_MESSAGE = "An error occurred while saving tasks to the file.";
+    private static final String INCOME_FILE_ACCESS_MESSAGE = "Error accessing file to save incomes" + "You should " +
+            "put the jar file in a folder with read and write access";
+    private static final String EXPENSE_FILE_ACCESS_MESSAGE = "Error accessing file to save expenses" + "You should " +
+            "put the jar file in a folder with read and write access";
+    private static final Logger LOGGER = Logger.getLogger(Parser.class.getName());
     private final String path;
+
+    static {
+        try {
+            File dir = new File("logs");
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    throw new KaChinnnngException("Failed to create directory " + dir.getAbsolutePath());
+                }
+            }
+            FileHandler fh = new FileHandler("logs/SaveToTxt.log", true);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            LOGGER.addHandler(fh);
+            LOGGER.setLevel(Level.ALL);
+            LOGGER.setUseParentHandlers(false);
+        } catch (SecurityException se) {
+            LOGGER.log(Level.SEVERE, "Error creating log file", se);
+            System.err.println("Insufficient permissions to create logs directory. Please check your permissions or " +
+                    "run the program in a different directory.");
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error creating log file", e);
+        }
+    }
 
     /**
      * Construct a new object of the SaveToTxt class with the file path.
@@ -54,11 +93,18 @@ public class SaveToTxt {
             for (Income income : incomes) {
                 String incomeDescription = income.getDescription();
                 String incomeDate = String.valueOf(income.getDate());
-                String incomeAmount = String.valueOf(income.getAmount());
-                String textToAdd = "I" + " | " + incomeDescription + " | " + incomeAmount + " | " + incomeDate + "\n";
+                String incomeAmount = df.format(income.getAmount());
+                String textToAdd = "I" + " |de " + incomeDescription + " |amt " + incomeAmount + " |date " +
+                        incomeDate + "\n";
                 fw.write(textToAdd);
+                LOGGER.log(Level.INFO , ("Income added to file: ") +
+                        incomeDescription + " with amount $ " + incomeAmount);
             }
+        } catch (AccessDeniedException e){
+            LOGGER.log(Level.WARNING, "Error accessing file ", e);
+            System.out.println(INCOME_FILE_ACCESS_MESSAGE);
         } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "Error accessing file ", e);
             System.out.println(IO_ERROR_MESSAGE);
         }
     }
@@ -76,22 +122,26 @@ public class SaveToTxt {
                 String textToAdd = "";
                 String expenseDescription = expense.getDescription();
                 String expenseDate = String.valueOf(expense.getDate());;
-                String expenseAmount = String.valueOf(expense.getAmount());
+                String expenseAmount = df.format(expense.getAmount());
 
                 // Determine the type of Expense and format the line accordingly
                 if (expense.getClass() == Food.class) {
-                    textToAdd = "EF" + " | " + expenseDescription + " | " + expenseAmount + " | "
-                            + expenseDate + " | " + ((Food) expense).getMealType().ordinal() + "\n";
+                    textToAdd = "EF" + " |de " + expenseDescription + " |amt " + expenseAmount + " |date "
+                            + expenseDate + " |type " + ((Food) expense).getMealType().ordinal() + "\n";
                 } else if (expense.getClass() == Transport.class) {
-                    textToAdd = "ET" + " | " + expenseDescription + " | " + expenseAmount + " | "
-                            + expenseDate + " | " + ((Transport) expense).getTransportationType().ordinal() + "\n";
+                    textToAdd = "ET" + " |de " + expenseDescription + " |amt " + expenseAmount + " |date "
+                            + expenseDate + " |type " + ((Transport) expense).getTransportationType().ordinal() + "\n";
                 } else if (expense.getClass() == Utilities.class) {
-                    textToAdd = "EU" + " | " + expenseDescription + " | " + expenseAmount + " | "
-                            + expenseDate + " | " + ((Utilities) expense).getUtilityType().ordinal() + "\n";
+                    textToAdd = "EU" + " |de " + expenseDescription + " |amt " + expenseAmount + " |date "
+                            + expenseDate + " |type " + ((Utilities) expense).getUtilityType().ordinal() + "\n";
                 }
                 fw.write(textToAdd);
             }
+        } catch (AccessDeniedException e){
+            LOGGER.log(Level.WARNING, "Error accessing file ", e);
+            System.out.println(EXPENSE_FILE_ACCESS_MESSAGE);
         } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "Error creating file ", e);
             System.out.println(IO_ERROR_MESSAGE);
         }
     }
