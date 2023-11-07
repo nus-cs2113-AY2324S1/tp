adc1d0b9f7485c32a4ffcbfe73b1e00e247025dc
Faris Sirraj <faris@curium.sg>
2023-11-08 00:56:03 +0800

    Add date as a parametre to calculate total steps

--- src/main/java/fittrack/command/TotalStepsCommand.java
+++ src/main/java/fittrack/command/TotalStepsCommand.java
@@ -1,5 +1,6 @@
 package fittrack.command;
 
+import fittrack.data.Date;
 import fittrack.data.Step;
 import fittrack.parser.CommandParser;
 import fittrack.parser.ParseException;
@@ -7,11 +8,14 @@ import fittrack.parser.ParseException;
 public class TotalStepsCommand extends Command{
     public static final String COMMAND_WORD = "totalsteps";
     private static final String DESCRIPTION = "`" + COMMAND_WORD + "` shows the total number of steps taken.";
-    private static final String USAGE = "Type `totalsteps` to show the total number of steps taken.";
+    private static final String USAGE = "Type `%s` <DATE> to show the total number of steps walked on that date.\n."+
+            "You should type <DATE> in format of `yyyy-MM-dd`.";
     public static final String HELP = DESCRIPTION + "\n" + USAGE;
 
-    private int totalSteps = 0;
-    private final CommandParser parser = new CommandParser();
+    private Date date;
+    private Step totalSteps;
+//    private int totalSteps = 0;
+
 
     public TotalStepsCommand(String commandLine) {
         super(commandLine);
@@ -19,10 +23,16 @@ public class TotalStepsCommand extends Command{
 
     @Override
     public CommandResult execute() {
+        StringBuilder feedbackBuilder = new StringBuilder();
+        totalSteps = new Step(0, null);
+
         for (Step step: stepList.getStepList()) {
-            totalSteps += step.getSteps();
+            if (date.equals(step.getDate())) {
+                totalSteps = totalSteps.sum(step);
+                feedbackBuilder.append(step).append("\n");
+            }
         }
-        return new CommandResult("Total steps taken: " + totalSteps + " steps");
+        return new CommandResult("Total steps taken: " + totalSteps.getSteps() + " steps");
     }
 
     /**
@@ -33,6 +43,7 @@ public class TotalStepsCommand extends Command{
      */
     @Override
     public void setArguments(String args) throws ParseException {
+        date = Date.parseDate(args);
     }
 
 
