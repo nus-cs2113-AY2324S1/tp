4f9085f66c25bf083cff4f5d7d28a7b35ba3c2c1
Martin Schneider <martin.schneider@ikmail.com>
2023-11-07 15:20:55 +0800

    Fix add duplicate drug

--- src/main/java/seedu/stocker/commands/AddCommand.java
+++ src/main/java/seedu/stocker/commands/AddCommand.java
@@ -16,30 +16,39 @@ public class AddCommand extends Command {
             + " /n Doliprane /d 12/06/2035 /s ABC123 /q 52 /p 12.90";
 
     public static final String MESSAGE_SUCCESS = "New drug added in the inventory: %1$s";
+    public static final String MESSAGE_FAILURE = "A different drugs already exists with differents properties. ";
 
-    private final Drug toAdd;
+    private final String name;
+    private final String expiryDate;
+    private final double sellingPrice;
     private final long quantity;
     private final String serialNumber;
 
     public AddCommand(String name, String expiryDate, String serialNumber, Long quantity, double sellingPrice) {
-        this.toAdd = new Drug(name, expiryDate, sellingPrice);
+        this.name = name;
+        this.expiryDate = expiryDate;
+        this.sellingPrice = sellingPrice;
         this.serialNumber = serialNumber;
         this.quantity = quantity;
-    }
-
-    public Drug getDrug() {
-        return this.toAdd;
     }
 
     @Override
     public <T> CommandResult<T> execute() {
         StockEntry entry = inventory.get(serialNumber);
         if (entry != null) {
-            entry.incrQuantity(this.quantity);
-            return new CommandResult<>(String.format(MESSAGE_SUCCESS, entry.getDrug().getName()));
+            Drug existingDrug = entry.getDrug();
+            if (existingDrug.getName().equals(this.name) &
+                    existingDrug.getExpiryDate().equals(this.expiryDate) &
+                    existingDrug.getSellingPrice() == this.sellingPrice) {
+                entry.incrQuantity(this.quantity);
+                return new CommandResult<>(String.format(MESSAGE_SUCCESS, entry.getDrug().getName()));
+            } else {
+                return new CommandResult<>(String.format(MESSAGE_FAILURE));
+            }
         } else {
-            inventory.addNewDrug(serialNumber, toAdd, quantity);
-            return new CommandResult<>(String.format(MESSAGE_SUCCESS, toAdd.getName()));
+            Drug newDrug = new Drug(this.name, this.expiryDate, this.sellingPrice);
+            inventory.addNewDrug(serialNumber, newDrug, quantity);
+            return new CommandResult<>(String.format(MESSAGE_SUCCESS, newDrug.getName()));
         }
     }
 }
