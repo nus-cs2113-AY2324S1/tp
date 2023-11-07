e7393d42d9c15feea3d01e82dcbb626bb8158094
Faris Sirraj <faris@curium.sg>
2023-11-07 19:13:53 +0800

    Update test with step functionality

--- src/test/java/fittrack/command/CaloriesBurntCommandTest.java
+++ src/test/java/fittrack/command/CaloriesBurntCommandTest.java
@@ -30,7 +30,7 @@ public class CaloriesBurntCommandTest {
     public void testExecute() throws ParseException {
         CaloriesBurntCommand caloriesBurntCommand = new CaloriesBurntCommand(CaloriesBurntCommand.COMMAND_WORD);
         caloriesBurntCommand.setArguments("2023-10-23", new CommandParser());
-        caloriesBurntCommand.setData(null, null, workoutList, null);
+        caloriesBurntCommand.setData(null, null, workoutList, null, null);
         CommandResult result = caloriesBurntCommand.execute();
         assertEquals("[W] workout1 (100kcal, 2023-10-23)\n" +
                         "[W] workout2 (200kcal, 2023-10-23)\n" +
