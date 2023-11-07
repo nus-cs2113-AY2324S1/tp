53e7a090d3049e5fc9fc69451faa3263ae5b8e75
Cazh1 <cazh1or2@gmail.com>
2023-11-08 00:40:29 +0800

    Add JUnit for PreviousDayCommand
    Fixes #286

---
+++ src/test/java/seedu/cafectrl/command/PreviousDayCommandTest.java
@@ -0,0 +1,33 @@
+package seedu.cafectrl.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.cafectrl.data.CurrentDate;
+import seedu.cafectrl.ui.Ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PreviousDayCommandTest {
+    @Test
+    public void execute_setTwoDayOneRun_expectDayOne() {
+        Ui ui = new Ui();
+        CurrentDate currentDate = new CurrentDate(2);
+        Command previousDayCommand = new PreviousDayCommand(ui, currentDate);
+        previousDayCommand.execute();
+        int actualDay = currentDate.getCurrentDay() + 1;
+        int expectedDay = 1;
+        assert actualDay == expectedDay;
+    }
+
+    @Test
+    public void execute_setTenDayNineRun_expectDayOne() {
+        Ui ui = new Ui();
+        CurrentDate currentDate = new CurrentDate(10);
+        for (int i = 0; i < 9; i++) {
+            Command previousDayCommand = new PreviousDayCommand(ui, currentDate);
+            previousDayCommand.execute();
+        }
+        int actualDay = currentDate.getCurrentDay() + 1;
+        int expectedDay = 1;
+        assert actualDay == expectedDay;
+    }
+}
