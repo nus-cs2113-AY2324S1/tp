2f9afb748f602f71ede432411130589be8322205
charkty <charkty@gmail.com>
2023-11-07 18:22:41 +0800

    Resolce conflicting start recipe message bug

--- src/main/java/essenmakanan/ui/Ui.java
+++ src/main/java/essenmakanan/ui/Ui.java
@@ -114,8 +114,8 @@ public class Ui {
                 diffUnitIngredients.listIngredients();
                 printNewLine();
             }
+            System.out.println("Start your recipe again after getting the above ingredients!");
         }
-        System.out.println("Start your recipe again after getting the above ingredients!");
         drawDivider();
     }
 
