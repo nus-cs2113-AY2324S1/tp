04f20aa5070657b742bdbee58f2eec742b38084c
yeo-menghan <yeomenghan1989@gmail.com>
2023-11-08 14:50:48 +0800

    Add protection against \exitquiz for shortans

--- src/main/java/quizhub/command/CommandShortAnswer.java
+++ src/main/java/quizhub/command/CommandShortAnswer.java
@@ -15,6 +15,7 @@ public class CommandShortAnswer extends Command {
     public static final String TOO_MANY_ARGUMENTS_MSG = "    Ono! There should not be arguments after /difficulty";
     public static final String DUPLICATED_INPUT = "    You have a duplicated input, please fill add a different input!";
     public static final String INVALID_DIFFICULTY_MSG = "    Invalid Difficulty, Entry will not be added to list!";
+    public static final String INVALID_EXITQUIZ = "    Invalid \\exitquiz as answer. Input a different answer!";
     public static final int ARGUMENT_SIZE = 4;
     private final String description;
     private final String answer;
