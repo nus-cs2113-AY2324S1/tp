27cb047fac5eff06039ced35d8266adfb96e3ef3
J0shuaLeong <joshualeong2607@gmail.com>
2023-11-09 16:29:54 +0800

    Organise methods and code

--- src/main/java/fittrack/FitTrack.java
+++ src/main/java/fittrack/FitTrack.java
@@ -55,25 +55,8 @@ public class FitTrack {
     private void start(String[] args) {
         ui.printVersion();
         ui.printWelcome();
-
-        try {
-            this.storage = initializeStorage(args);
-            if (!storage.isProfileFileEmpty()) {
-                this.userProfile = storage.profileLoad();
-                ui.printPrompt();
-                isValidInput = true;
-            }
-            this.mealList = storage.mealLoad();
-            this.workoutList = storage.workoutLoad();
-            this.stepList = storage.stepLoad();
-        } catch (StorageOperationException | InvalidStorageFilePathException e) {
-            System.out.println("There was a problem with the loading of storage contents.");
-            ui.printLine();
-        }
-
-        while (!isValidInput) {
-            startProfile();
-        }
+        loadStorage(args);
+        startProfile();
     }
 
     private void loopCommandExecution() {
@@ -100,22 +83,40 @@ public class FitTrack {
     }
 
     private void startProfile() {
-        try {
-            String input = ui.profileMessageAndScanner();
-            userProfile.profileSettings(input);
-            ui.printProfileDetails(userProfile);
-            storage.saveProfile(userProfile);
-            isValidInput = true;
-        } catch (PatternMatchFailException e) {
-            System.out.println("Wrong format. Please enter h/<height> w/<weight> g/<gender> l/<dailyCalorieLimit>");
-        } catch (IOException e) {
-            System.out.println("Error occurred while saving profile.");
-            isValidInput = true;
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
+        while (!isValidInput) {
+            try {
+                String input = ui.profileMessageAndScanner();
+                userProfile.profileSettings(input);
+                ui.printProfileDetails(userProfile);
+                storage.saveProfile(userProfile);
+                isValidInput = true;
+            } catch (PatternMatchFailException e) {
+                System.out.println("Wrong format. Please enter h/<height> w/<weight> g/<gender> l/<dailyCalorieLimit>");
+            } catch (IOException e) {
+                System.out.println("Error occurred while saving profile.");
+                isValidInput = true;
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
+    private void loadStorage(String[] args) {
+        try {
+            this.storage = initializeStorage(args);
+            if (!storage.isProfileFileEmpty()) {
+                this.userProfile = storage.profileLoad();
+                ui.printPrompt();
+                isValidInput = true;
+            }
+            this.mealList = storage.mealLoad();
+            this.workoutList = storage.workoutLoad();
+            this.stepList = storage.stepLoad();
+        } catch (StorageOperationException | InvalidStorageFilePathException e) {
+            System.out.println("There was a problem with the loading of storage contents.");
+            ui.printLine();
+        }
+    }
 
     /**
      * Creates the StorageFile object based on the user specified path (if any) or the default storage path.
