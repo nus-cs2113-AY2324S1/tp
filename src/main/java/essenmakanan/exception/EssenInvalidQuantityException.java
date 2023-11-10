2c2eefbd8c921ab107504ce22dee2184567e9847
Stanley Wijaya <wijayastanley00@gmail.com>
2023-11-10 14:32:00 +0800

    Update exception message

--- src/main/java/essenmakanan/exception/EssenInvalidQuantityException.java
+++ src/main/java/essenmakanan/exception/EssenInvalidQuantityException.java
@@ -3,6 +3,6 @@ package essenmakanan.exception;
 public class EssenInvalidQuantityException extends Exception {
 
     public static void handleException() {
-        System.out.println("Invalid number");
+        System.out.println("Please put a non-zero positive number as the quantity");
     }
 }
--- src/main/java/essenmakanan/exception/EssenShortcutException.java
+++ src/main/java/essenmakanan/exception/EssenShortcutException.java
@@ -3,6 +3,6 @@ package essenmakanan.exception;
 public class EssenShortcutException extends Exception {
 
     public void handleException() {
-
+        System.out.println("Shortcut cannot be created for a non-existing ingredient");
     }
 }
