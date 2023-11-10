156d922c5c66b4b6d8ab8a7972b997d45667d714
kai jie <leowkaijie@gmail.com>
2023-11-10 19:55:30 +0800

    Test for view invalid recipe

--- src/test/java/essenmakanan/parser/RecipeParserTest.java
+++ src/test/java/essenmakanan/parser/RecipeParserTest.java
@@ -1,6 +1,9 @@
 package essenmakanan.parser;
 
+import essenmakanan.command.ViewSpecificRecipeCommand;
 import essenmakanan.exception.EssenException;
+import essenmakanan.exception.EssenOutOfRangeException;
+import essenmakanan.recipe.Recipe;
 import essenmakanan.recipe.RecipeList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,8 +17,15 @@ public class RecipeParserTest {
 
     @BeforeEach
     public void setUp() {
+
         recipeParser = new RecipeParser();
         recipes = new RecipeList();
+
+        String[] recipeSteps = {"step1", "step2"};
+        String[] recipeIngredients = {"i/flour,200,g", "i/egg,2,pc"};
+        Recipe banana = new Recipe("banana", recipeSteps, recipeIngredients);
+
+        recipes.addRecipe(banana);
     }
 
     @Test
@@ -24,6 +34,14 @@ public class RecipeParserTest {
         String invalidDetails = "recipes in my house";
         assertThrows(EssenException.class, () -> {
             recipeParser.parseRecipeCommand(recipes, invalidCommand, invalidDetails);
+        });
+    }
+
+    @Test
+    public void viewRecipe_invalidRecipe_errorThrown() {
+        String input = "r/apple";
+        assertThrows(EssenOutOfRangeException.class, () -> {
+            RecipeParser.getRecipeIndex(recipes, input);
         });
     }
 
