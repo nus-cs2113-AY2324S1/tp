67725e68b9de2e3e9a93acccb92f5f9b81ef58c9
Stanley Wijaya <wijayastanley00@gmail.com>
2023-11-10 16:17:50 +0800

    Update UI exception

--- src/main/java/essenmakanan/exception/EssenShortcutException.java
+++ src/main/java/essenmakanan/exception/EssenShortcutException.java
@@ -1,8 +1,12 @@
 package essenmakanan.exception;
 
+import essenmakanan.ui.Ui;
+
 public class EssenShortcutException extends Exception {
 
     public void handleException() {
+        Ui.drawDivider();
         System.out.println("Shortcut cannot be created for a non-existing ingredient");
+        Ui.drawDivider();
     }
 }
