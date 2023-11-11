15a99f1ea2f4b4b2a64217e67b3e9a1bb964cacf
Stanley Wijaya <wijayastanley00@gmail.com>
2023-11-12 00:06:45 +0800

    Fix checktstyle

--- src/test/java/essenmakanan/storage/StorageTest.java
+++ src/test/java/essenmakanan/storage/StorageTest.java
@@ -10,8 +10,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 public class StorageTest {
 
