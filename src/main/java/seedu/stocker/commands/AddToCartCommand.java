9380de90ca2bf7ecdb41ed96caaf11bd76dc14f7
barbaracwx <barbara9668@gmail.com>
2023-11-11 15:48:04 +0800

    add such that if past expiry date, unable to add to cart

--- src/main/java/seedu/stocker/commands/AddToCartCommand.java
+++ src/main/java/seedu/stocker/commands/AddToCartCommand.java
@@ -2,6 +2,8 @@ package seedu.stocker.commands;
 
 import seedu.stocker.drugs.CartEntry;
 import seedu.stocker.drugs.StockEntry;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Adds a certain quantity of one drug into the current cart
@@ -10,7 +12,7 @@ public class AddToCartCommand extends Command {
 
     public static final String COMMAND_WORD = "addToCart";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Adds a new drug to the current cart. "
             + "Parameters: SERIAL NUMBER, QUANTITY," + System.lineSeparator()
             + "Example: " + COMMAND_WORD
@@ -31,18 +33,37 @@ public class AddToCartCommand extends Command {
         StockEntry entry = inventory.get(this.serialNumber);
         if (entry == null) {
             return new CommandResult<>("This drug is not in stock");
-        } else if (entry.getQuantity() < this.quantity
-                + currentCart.getEntryQuantity(this.serialNumber)) {
-            return new CommandResult<>("There is not enough stock on this drug. ");
         } else {
-            CartEntry cartEntry = currentCart.getEntryBySerialNumber(this.serialNumber);
-            if (cartEntry == null) {
-                currentCart.addEntry(this.serialNumber, this.quantity);
+            String inputDate = entry.getDrug().getExpiryDate();
+            LocalDate expiryDate = parseDate(inputDate);
+            LocalDate currentDate = LocalDate.now(); // Get the current date
+
+            if (currentDate.isAfter(expiryDate)) {
+                return new CommandResult<>("This drug is expired. Unable to add to cart");
+            } else if (entry.getQuantity() < this.quantity + currentCart.getEntryQuantity(this.serialNumber)) {
+                return new CommandResult("There is not enough stock on this drug.");
             } else {
-                cartEntry.incrQuantity(this.quantity);
+                CartEntry cartEntry = currentCart.getEntryBySerialNumber(this.serialNumber);
+                if (cartEntry == null) {
+                    currentCart.addEntry(this.serialNumber, this.quantity);
+                } else {
+                    cartEntry.incrQuantity(this.quantity);
+                }
+                return new CommandResult(String.format(MESSAGE_SUCCESS, entry.getDrug().getName()));
             }
-            return new CommandResult<>(String.format(MESSAGE_SUCCESS, entry.getDrug().getName()));
         }
     }
+
+    /**
+     * Helper method to parse a date string in "dd/mm/yyyy" format to a LocalDate.
+     *
+     * @param dateStr The input date string.
+     * @return The parsed LocalDate.
+     */
+    private LocalDate parseDate(String dateStr) {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return LocalDate.parse(dateStr, inputFormatter);
+    }
+
 
 }
