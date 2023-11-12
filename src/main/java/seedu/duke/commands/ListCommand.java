0afaca096ba505cf17046df133d3ec4fb6c896f0
KenCheung18 <kycheungbf@connect.ust.hk>
2023-11-12 17:40:59 +0800

    Change List commands to ui.print for testing reason

--- src/main/java/seedu/duke/commands/ListCommand.java
+++ src/main/java/seedu/duke/commands/ListCommand.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class ListCommand extends Command {
     private ArrayList<Income> incomes;
     private ArrayList<Expense> expenses;
+    private Ui ui;
 
     private double totalIncome;
     private double totalExpenses;
@@ -23,11 +24,13 @@ public class ListCommand extends Command {
      *
      * @param incomes   The list of income records to be displayed.
      * @param expenses  The list of expense records to be displayed.
+     * @param ui        The user interface for displaying messages and financial records.
      */
 
-    public ListCommand(ArrayList<Income> incomes, ArrayList<Expense> expenses) {
+    public ListCommand(ArrayList<Income> incomes, ArrayList<Expense> expenses, Ui ui) {
         this.incomes = incomes;
         this.expenses = expenses;
+        this.ui = ui;
         totalIncome = 0;
         totalExpenses = 0;
     }
@@ -39,16 +42,16 @@ public class ListCommand extends Command {
     @Override
     public void execute() {
         if (incomes.isEmpty() && expenses.isEmpty()) {
-            System.out.println("You do not have any records.");
+            ui.printMessage("You do not have any records.");
             return;
         }
         // Print incomes
         if (incomes.isEmpty()) {
-            System.out.println("You have no recorded incomes.");
+            ui.printMessage("You have no recorded incomes.");
         } else {
-            System.out.println("Here are your incomes:");
+            ui.printMessage("Here are your incomes:");
             for (int i = 0; i < incomes.size(); i++) {
-                System.out.println((i + 1) + ". " + incomes.get(i).toString());
+                ui.printMessage((i + 1) + ". " + incomes.get(i).toString());
                 totalIncome += incomes.get(i).getAmount();
             }
             System.out.printf("Total income is: $%.2f.\n", totalIncome);
@@ -56,11 +59,11 @@ public class ListCommand extends Command {
         System.out.println();
         // Print expenses
         if (expenses.isEmpty()) {
-            System.out.println("You have no recorded expenses.");
+            ui.printMessage("You have no recorded expenses.");
         } else {
-            System.out.println("Here are your expenses:");
+            ui.printMessage("Here are your expenses:");
             for (int i = 0; i < expenses.size(); i++) {
-                System.out.println((i + 1) + ". " + expenses.get(i).toString());
+                ui.printMessage((i + 1) + ". " + expenses.get(i).toString());
                 totalExpenses += expenses.get(i).getAmount();
             }
             System.out.printf("Total expenses is: $%.2f.\n", totalExpenses);
