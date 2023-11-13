655658556e4d743e24b1d1842d6dac249a368791
ziyi105 <82555990+ziyi105@users.noreply.github.com>
2023-11-13 15:17:30 +0800

    Update src/main/java/seedu/cafectrl/data/CurrentDate.java
    Co-authored-by: Dexter <35828587+DextheChik3n@users.noreply.github.com>

--- src/main/java/seedu/cafectrl/data/CurrentDate.java
+++ src/main/java/seedu/cafectrl/data/CurrentDate.java
@@ -3,8 +3,10 @@ package seedu.cafectrl.data;
 import java.util.ArrayList;
 
 public class CurrentDate {
-    private static final int ZERO = 0;
-    private static final int ONE = 1;
+    private static final int MIN_ORDER_LIST_SIZE = 0;
+    private static final int DAY_BASE_NUMBER = 0;
+    private static final int DAY_OFFSET = 1;
+    private static final int ORDER_LIST_SIZE_OFFSET = 1;
     private int currentDay;
 
     public CurrentDate() {
