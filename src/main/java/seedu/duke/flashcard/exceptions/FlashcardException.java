8e62ec55e5089b61ec1722ce6c40eaeb78f58278
Wendelin Wemhoener <wendelin.wemhoener@gmail.com>
2023-11-13 16:52:25 +0800

    Add more exception-handling

---
+++ src/main/java/seedu/duke/flashcard/exceptions/FlashcardException.java
@@ -0,0 +1,9 @@
+//@@author wendelinwemhoener
+
+package seedu.duke.flashcard.exceptions;
+
+/**
+ * Base class for all custom exceptions related to flashcards.
+ */
+public class FlashcardException extends Exception {
+}
