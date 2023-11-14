7e6e5c82b44c245c61c959635a3b2677a014476d
MSI\jyben <jybentian@gmail.com>
2023-11-14 20:25:13 +0800

    Recover all goal command executions

--- src/main/java/seedu/duke/commands/goal/AchieveGoalCommand.java
+++ src/main/java/seedu/duke/commands/goal/AchieveGoalCommand.java
@@ -1,6 +1,10 @@
 package seedu.duke.commands.goal;
 
 import seedu.duke.commands.Command;
+import seedu.duke.commands.CommandResult;
+
+import seedu.duke.data.GoalList;
+import seedu.duke.data.exception.IncorrectFormatException;
 
 public class AchieveGoalCommand extends Command {
     public static final String COMMAND_WORD = "achieve";
@@ -11,4 +15,21 @@ public class AchieveGoalCommand extends Command {
     public AchieveGoalCommand(String cmd) {
         super(cmd);
     }
+
+    @Override
+    public CommandResult execute() {
+        try {
+            feedbackToUser = GoalList.achieveGoal(this.userCommand);
+
+        } catch (NumberFormatException nfe) {
+            feedbackToUser = "Please use a valid arabic number as index.";
+        } catch (IncorrectFormatException ife) {
+            feedbackToUser = ife.getMessage();
+        } catch (Exception e) {
+            feedbackToUser = "Something went wrong, please try again.";
+        }
+
+        return new CommandResult(feedbackToUser);
+    }
+
 }
--- src/main/java/seedu/duke/commands/goal/ViewGoalCommand.java
+++ src/main/java/seedu/duke/commands/goal/ViewGoalCommand.java
@@ -1,6 +1,10 @@
 package seedu.duke.commands.goal;
 
 import seedu.duke.commands.Command;
+import seedu.duke.commands.CommandResult;
+import seedu.duke.data.GoalList;
+import seedu.duke.data.exception.IncorrectFormatException;
+import seedu.duke.ui.TextUi;
 
 public class ViewGoalCommand extends Command {
 
@@ -12,4 +16,19 @@ public class ViewGoalCommand extends Command {
     public ViewGoalCommand(String cmd) {
         super(cmd);
     }
+
+    @Override
+    public CommandResult execute() {
+        try {
+            GoalList.verifyViewGoalInput(this.userCommand);
+            feedbackToUser = TextUi.showGoalList();
+        } catch (IncorrectFormatException ife) {
+            feedbackToUser = ife.getMessage();
+        } catch (Exception e) {
+            feedbackToUser = "Something went wrong, please try again.";
+        }
+
+        return new CommandResult(feedbackToUser);
+    }
+
 }
